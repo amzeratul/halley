d753f1541a76af915bc83dc31d7fd14ca5c98121
Rodrigo Braz Monteiro <archmagezeratul@gmail.com>
2016-02-21 21:47:43 +0000

    Utils include header cleanup.

--- utils/include/halley_utils.h
+++ utils/include/halley_utils.h
@@ -1,29 +1,3 @@
 #pragma once
 
-#include "../src/maths/aabb.h"
-#include "../src/maths/angle.h"
-#include "../src/maths/base_transform.h"
-#include "../src/maths/box.h"
-#include "../src/maths/colour.h"
-#include "../src/maths/fixed_point.h"
-#include "../src/maths/polygon.h"
-#include "../src/maths/random.h"
-#include "../src/maths/range.h"
-#include "../src/maths/rect.h"
-#include "../src/maths/utils.h"
-#include "../src/maths/vector2d.h"
-#include "../src/maths/vector3d.h"
-
-#include "../src/debug/assert.h"
-#include "../src/debug/console.h"
-#include "../src/debug/debug.h"
-#include "../src/debug/exception.h"
-
-#include "../src/text/halleystring.h"
-
-#include "../src/time/halleytime.h"
-
-#include "../src/data_structures/memory_pool.h"
-#include "../src/data_structures/circular_buffer.h"
-
-#include "../src/os/os.h"
+#include "../src/includes.h"
---
+++ utils/src/includes.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include "maths/aabb.h"
+#include "maths/angle.h"
+#include "maths/base_transform.h"
+#include "maths/box.h"
+#include "maths/colour.h"
+#include "maths/fixed_point.h"
+#include "maths/polygon.h"
+#include "maths/random.h"
+#include "maths/range.h"
+#include "maths/rect.h"
+#include "maths/utils.h"
+#include "maths/vector2d.h"
+#include "maths/vector3d.h"
+
+#include "debug/assert.h"
+#include "debug/console.h"
+#include "debug/debug.h"
+#include "debug/exception.h"
+
+#include "data_structures/circular_buffer.h"
+#include "data_structures/mapped_pool.h"
+#include "data_structures/memory_pool.h"
+
+#include "concurrency/concurrent.h"
+#include "concurrency/jobs.h"
+#include "concurrency/thread_pool.h"
+
+#include "file_formats/image.h"
+#include "file_formats/ini_reader.h"
+#include "file_formats/json_file.h"
+#include "file_formats/xml_file.h"
+#include "file_formats/serializer.h"
+#include "file_formats/string_serializer.h"
+#include "file_formats/text_reader.h"
+
+#include "text/encode.h"
+#include "text/halleystring.h"
+
+#include "time/halleytime.h"
+#include "time/stopwatch.h"
+
+#include "web/http.h"
+
+#include "os/os.h"
