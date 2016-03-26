96ade7b1f90f3e62ad062b784d3cb740a888a3c1
Rodrigo Braz Monteiro <archmagezeratul@gmail.com>
2016-03-26 23:42:21 +0000

    WIP handle for masks...

--- entity/src/family_mask.cpp
+++ entity/src/family_mask.cpp
@@ -1,63 +1,128 @@
 #include "family_mask.h"
+#include <set>
 
 using namespace Halley;
+using namespace FamilyMask;
 
-FamilyMask::Handle::Handle()
+struct MaskEntry
+{
+	RealType mask;
+	int idx;
+
+	MaskEntry(MaskEntry&& o)
+		: mask(std::move(o.mask))
+		, idx(o.idx)
+	{}
+
+	MaskEntry(const RealType& m, int i)
+		: mask(m)
+		, idx(i)
+	{}
+
+	bool operator<(const MaskEntry& o) const {
+		return mask < o.mask;
+	}
+
+	bool operator==(const MaskEntry& o) const {
+		return mask == o.mask;
+	}
+};
+
+class MaskStorage
+{
+public:
+	static MaskStorage& getInstance()
+	{
+		static MaskStorage* i = nullptr;
+		if (!i) {
+			i = new MaskStorage();
+		}
+		return *i;
+	}
+
+	std::vector<MaskEntry*> values;
+	std::set<MaskEntry> entries;
+
+	static int getHandle(RealType& value)
+	{
+		auto& instance = getInstance();
+		auto entry = MaskEntry(value, 0);
+		auto i = instance.entries.find(entry);
+		if (i == instance.entries.end()) {
+			// Not found
+			entry.idx = static_cast<int>(instance.values.size());
+			auto result = instance.entries.insert(std::move(entry));
+			instance.values.push_back(const_cast<MaskEntry*>(&*result.first));
+			return entry.idx;
+		} else {
+			// Found
+			return i->idx;
+		}
+	}
+
+	static RealType& retrieve(int handle)
+	{
+		return getInstance().values[handle]->mask;
+	}
+};
+
+
+Handle::Handle()
 	: value(0)
 {
 }
 
-FamilyMask::Handle::Handle(const Handle& h)
+Handle::Handle(const Handle& h)
 	: value(h.value)
 {
 }
 
-FamilyMask::Handle::Handle(Handle&& h)
+Handle::Handle(Handle&& h)
 	: value(h.value)
 {
 }
 
-FamilyMask::Handle::Handle(const RealType& mask)
+Handle::Handle(const RealType& mask)
 	: value(mask)
 {
 }
 
-FamilyMask::Handle::Handle(RealType&& mask)
+Handle::Handle(RealType&& mask)
 	: value(mask)
 {
 }
 
-void FamilyMask::Handle::operator=(const Handle& h)
+void Handle::operator=(const Handle& h)
 {
 	value = h.value;
 }
 
-bool FamilyMask::Handle::operator==(const Handle& h) const
+bool Handle::operator==(const Handle& h) const
 {
 	return value == h.value;
 }
 
-bool FamilyMask::Handle::operator!=(const Handle& h) const
+bool Handle::operator!=(const Handle& h) const
 {
 	return value != h.value;
 }
 
-bool FamilyMask::Handle::operator<(const Handle& h) const
+bool Handle::operator<(const Handle& h) const
 {
 	return value < h.value;
 }
 
-FamilyMask::Handle FamilyMask::Handle::operator&(const Handle& h) const
+Handle Handle::operator&(const Handle& h) const
 {
 	return Handle(value & h.value);
 }
 
-const FamilyMask::RealType& FamilyMask::Handle::getRealValue() const
+const RealType& Handle::getRealValue() const
 {
 	return value;
 }
 
-FamilyMask::HandleType FamilyMask::getHandle(RealType mask)
+HandleType FamilyMask::getHandle(RealType mask)
 {
 	return Handle(mask);
 }
