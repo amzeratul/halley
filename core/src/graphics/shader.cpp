07876f6a489501fedde18cc9b05e1644cbf1d6cf
Rodrigo Braz Monteiro <archmagezeratul@gmail.com>
2016-04-06 22:08:37 +0100

    Updated todo

--- core/src/graphics/shader.cpp
+++ core/src/graphics/shader.cpp
@@ -15,11 +15,13 @@ std::unique_ptr<Shader> Shader::loadResource(ResourceLoader& loader)
 
 	auto data = loader.getStatic();
 	auto shaderStrData = data->getString();
+	// TODO: parse
 
 	String vertex;
 	String pixel;
 	String geometry;
 
+	// TODO: read these from the right place
 	vertex = api.core->getResources().get<TextFile>("shaders/sprite.vertex.glsl")->data;
 	pixel = api.core->getResources().get<TextFile>("shaders/sprite.pixel.glsl")->data;
 
