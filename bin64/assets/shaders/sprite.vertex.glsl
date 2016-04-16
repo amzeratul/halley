ef46102da5ad3f4e48b8b7c7f797e73a54e2fc38
Rodrigo Braz Monteiro <archmagezeratul@gmail.com>
2016-04-16 19:12:07 +0100

    Refactored sprite pixel shader.

--- bin64/assets/shaders/sprite.vertex.glsl
+++ bin64/assets/shaders/sprite.vertex.glsl
@@ -12,20 +12,29 @@ out vec4 v_texCoord0;
 out vec4 v_color;
 out vec4 v_colorAdd;
 
-void main() {
-	vec2 texPos = mix(a_vertPos, vec2(1.0 - a_vertPos.y, a_vertPos.x), a_size.w);
-	v_texCoord0 = vec4(mix(a_texCoord0.xy, a_texCoord0.zw, texPos.xy), 0.0, 0.0);
-	
-	vec4 inputCol = vec4(a_color.rgb * a_color.a, a_color.a); // Premultiply alpha
+vec4 getTexCoord(vec4 texCoords, vec2 vertPos, float texCoordRotation) {
+	vec2 texPos = mix(vertPos, vec2(1.0 - vertPos.y, vertPos.x), texCoordRotation);
+	return vec4(mix(texCoords.xy, texCoords.zw, texPos.xy), 0.0, 0.0);
+}
+
+void getColours(vec4 inColour, out vec4 baseColour, out vec4 addColour) {
+	vec4 inputCol = vec4(inColour.rgb * inColour.a, inColour.a); // Premultiply alpha
 	vec4 baseCol = clamp(inputCol, vec4(0, 0, 0, 0), vec4(1, 1, 1, 1));
-	v_color = baseCol;
-	v_colorAdd = clamp(inputCol - baseCol, vec4(0, 0, 0, 0), vec4(1, 1, 1, 0));
-	
-	float angle = a_size.z;
+	baseColour = baseCol;
+	addColour = clamp(inputCol - baseCol, vec4(0, 0, 0, 0), vec4(1, 1, 1, 0));
+}
+
+vec4 getVertexPosition(vec2 position, vec2 pivot, vec2 size, vec2 vertPos, float angle) {
 	float c = cos(angle);
 	float s = sin(angle);
 	mat2 m = mat2(c, s, -s, c);
 	
-	vec2 pos = a_position.xy + m * ((a_vertPos - a_position.zw) * a_size.xy);
-	gl_Position = u_mvp * vec4(pos.x, pos.y, 0.0, 1.0);
+	vec2 pos = position + m * ((vertPos - pivot) * size);
+	return u_mvp * vec4(pos, 0.0, 1.0);
+}
+
+void main() {
+	v_texCoord0 = getTexCoord(a_texCoord0, a_vertPos, a_size.w);
+	getColours(a_color, v_color, v_colorAdd);
+	gl_Position = getVertexPosition(a_position.xy, a_position.zw, a_size.xy, a_vertPos, a_size.z);
 }
