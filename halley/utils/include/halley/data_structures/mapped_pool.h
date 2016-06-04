ed8efec73fe350fa630c9990be1a753aeeb4b258
Rodrigo Braz Monteiro <archmagezeratul@gmail.com>
2016-06-04 21:30:07 +0100

    Clean up of Mapped Pool

--- halley/utils/include/halley/data_structures/mapped_pool.h
+++ halley/utils/include/halley/data_structures/mapped_pool.h
@@ -26,50 +26,59 @@ namespace Halley {
 	class MappedPool {
 		struct Entry {
 			std::array<char, sizeof(T)> data;
-			unsigned short index;
+			unsigned short nextFreeEntryIndex;
 			unsigned short revision;
 		};
 
 		struct Block {
 			Vector<Entry> data;
 
-			Block(size_t n)
+			Block(size_t blockIndex)
 				: data(blockLen)
 			{
-				size_t base = n * blockLen;
+				size_t base = blockIndex * blockLen;
 				for (size_t i = 0; i < blockLen - 1; i++) {
-					data[i].index = static_cast<unsigned short>(i + 1 + base);
+					// Each entry points to the next
+					data[i].nextFreeEntryIndex = static_cast<unsigned short>(i + 1 + base);
 					data[i].revision = 0;
 				}
-				data[blockLen - 1].index = 0xFFFF;
+				data[blockLen - 1].nextFreeEntryIndex = 0xFFFF;
 				data[blockLen - 1].revision = 0;
 			}
 		};
 
 	public:
 		std::pair<T*, int> alloc() {
-			if (next == 0xFFFF) {
+			// Next entry will be at position "entryIdx", which is just what was stored on next
+			int entryIdx = next;
+
+			// Figure which block it goes into, and make sure that exists
+			int blockIdx = entryIdx / blockLen;
+			if (blockIdx >= blocks.size()) {
 				blocks.push_back(Block(blocks.size()));
-				next = static_cast<unsigned short>((blocks.size() - 1) * blockLen);
 			}
-			int blockIdx = next;
-			auto& block = blocks[blockIdx / blockLen];
-			int localIdx = blockIdx % blockLen;
+			auto& block = blocks[blockIdx];
+
+			// Find the local entry inside that block and initialize it
+			int localIdx = entryIdx % blockLen;
 			auto& data = block.data[localIdx];
 			int rev = data.revision;
 			T* result = reinterpret_cast<T*>(&(data.data));
 
-			// Next block is what was stored on the index
-			// Index is now used to store the external index of the current block, so update that
-			std::swap(next, block.data[localIdx].index);
+			// Next block is what was stored on the nextFreeEntryIndex
+			std::swap(next, block.data[localIdx].nextFreeEntryIndex);
 
-			int externalIdx = static_cast<signed int>(blockIdx & 0xFFFF) | (static_cast<signed int>(rev & 0x7FFFF) << 16); // TODO: compute properly
+			// External index composes the revision with the index, so it's unique, but easily mappable
+			int externalIdx = static_cast<signed int>(entryIdx & 0xFFFF) | (static_cast<signed int>(rev & 0x7FFF) << 16); // TODO: compute properly
 			return std::pair<T*, int>(result, externalIdx);
 		}
 
 		void free(T* p) {
+			// Swaps the data with the next, so this will actually be the next one to be allocated
 			Entry* entry = reinterpret_cast<Entry*>(p);
-			std::swap(entry->index, next);
+			std::swap(entry->nextFreeEntryIndex, next);
+
+			// Increase revision so the next one to allocate this gets a unique number
 			++entry->revision;
 		}
 
@@ -85,6 +94,9 @@ namespace Halley {
 			if (blockN < 0 || blockN >= signed(blocks.size())) {
 				return nullptr;
 			}
+
+			// TODO: check if can shrink?
+
 			auto& block = blocks[blockN];
 			int localIdx = idx % blockLen;
 			auto& data = block.data[localIdx];
@@ -96,6 +108,6 @@ namespace Halley {
 
 	private:
 		Vector<Block> blocks;
-		unsigned short next = 0xFFFF;
+		unsigned short next = 0;
 	};
 }
