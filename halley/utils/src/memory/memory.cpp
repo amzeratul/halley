f9ce194d954c4e2e58f1bf481bfa1c2c1f1c9ad5
Rodrigo Braz Monteiro <archmagezeratul@gmail.com>
2016-06-04 16:09:18 +0100

    Clean-up.

--- halley/utils/src/memory/memory.cpp
+++ halley/utils/src/memory/memory.cpp
@@ -1,4 +1,10 @@
+#if HAS_EASTL
+
+#ifdef _MSC_VER
 #include <malloc.h>
+#else
+#include <stdlib.h>
+#endif
 
 void* operator new[](size_t size, const char* /*name*/, int /*flags*/, unsigned /*debugFlags*/, const char* /*file*/, int /*line*/)
 {
@@ -7,5 +13,13 @@ void* operator new[](size_t size, const char* /*name*/, int /*flags*/, unsigned 
 
 void* operator new[](size_t size, size_t alignment, size_t /*alignmentOffset*/, const char* /*name*/, int /*flags*/, unsigned /*debugFlags*/, const char* /*file*/, int /*line*/)
 {
+#ifdef _MSC_VER
 	return _aligned_malloc(size, alignment);
+#else
+	void* result;
+	posix_memalign(&result, alignment, size);
+	return result;
+#endif
 }
+
+#endif
