f1ae3f40532123c822792b19efc1fffba92f0d7b
Rodrigo Braz Monteiro <archmagezeratul@gmail.com>
2016-08-04 17:10:45 +0100

    Change texture loading so concurrency can be disabled.

--- halley/core/src/graphics/texture.cpp
+++ halley/core/src/graphics/texture.cpp
@@ -11,25 +11,22 @@ std::unique_ptr<Texture> Texture::loadResource(ResourceLoader& loader)
 	Metadata meta = loader.getMeta();
 	auto video = loader.getAPI().video;
 
-	auto future = loader.getAsync().then([meta] (std::unique_ptr<ResourceDataStatic> data) -> std::unique_ptr<Image>
+	auto loadImage = [meta](std::unique_ptr<ResourceDataStatic> data) -> std::unique_ptr<Image>
 	{
 		return std::make_unique<Image>(data->getPath(), static_cast<Byte*>(data->getData()), data->getSize(), meta.getBool("premultiply", true));
-	}).then(Executors::getMainThread(), [meta, video] (std::unique_ptr<Image> img) -> std::unique_ptr<Texture>
+	};
+
+	auto loadTexture = [meta, video](std::unique_ptr<Image> img) -> std::unique_ptr<Texture>
 	{
 		TextureDescriptor descriptor(Vector2i(img->getWidth(), img->getHeight()));
 		descriptor.pixelData = img->getPixels();
-
 		descriptor.useFiltering = meta.getBool("filtering", false);
 		descriptor.useMipMap = meta.getBool("mipmap", false);
 		descriptor.format = meta.getString("format", "RGBA") == "RGBA" ? TextureFormat::RGBA : TextureFormat::RGB;
 
 		return video->createTexture(descriptor);
-	});
+	};
 
-	Executor e(Executors::getMainThread());
-	while (!future.isReady()) {
-		e.runPending();
-	}
-
-	return future.get();
+	//return loader.getAsync().then(loadImage).then(Executors::getVideoAux(), loadTexture);
+	return loadTexture(loadImage(loader.getStatic()));
 }
