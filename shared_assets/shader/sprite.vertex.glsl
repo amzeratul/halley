7aae64ffd543c399527db037d57b4f95768d002d
Rodrigo Braz Monteiro <archmagezeratul@gmail.com>
2016-11-17 13:45:59 +0000

    Sprites now pass the vertPos forward.

--- shared_assets/shader/sprite.vertex.glsl
+++ shared_assets/shader/sprite.vertex.glsl
@@ -16,6 +16,7 @@ out vec2 v_texCoord0;
 out vec2 v_pixelTexCoord0;
 out vec4 v_colour;
 out vec4 v_colourAdd;
+out vec2 v_vertPos;
 
 vec2 getTexCoord(vec4 texCoords, vec2 vertPos, float texCoordRotation) {
 	vec2 texPos = mix(vertPos, vec2(1.0 - vertPos.y, vertPos.x), texCoordRotation);
@@ -41,6 +42,7 @@ vec4 getVertexPosition(vec2 position, vec2 pivot, vec2 size, vec2 vertPos, float
 void main() {
 	v_texCoord0 = getTexCoord(a_texCoord0, a_vertPos.zw, a_textureRotation);
 	v_pixelTexCoord0 = v_texCoord0 * a_size;
+	v_vertPos = a_vertPos.xy;
 	getColours(a_colour, v_colour, v_colourAdd);
 	gl_Position = getVertexPosition(a_position, a_pivot, a_size * a_scale, a_vertPos.xy, a_rotation);
 }
