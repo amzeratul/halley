a7710a3bd4b7cd75e514e580b4215fbfce5e1745
Rodrigo Braz Monteiro <archmagezeratul@gmail.com>
2017-01-06 09:57:52 +0000

    Renamed to getScaledSize.

--- engine/core/include/halley/core/graphics/sprite/sprite.h
+++ engine/core/include/halley/core/graphics/sprite/sprite.h
@@ -43,7 +43,7 @@ namespace Halley
 
 		Rect4f getAABB() const;
 		bool isInView(Rect4f rect) const;
-		Vector2f getSize() const;
+		Vector2f getScaledSize() const;
 		
 		Sprite& setMaterial(Resources& resources, String materialName = "");
 		Sprite& setMaterial(std::shared_ptr<Material> m);
