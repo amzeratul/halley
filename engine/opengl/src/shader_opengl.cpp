amzeratul/halley

commit 3dfc4903c92bab6b1b3b7d040dc2033fcd9d5a41

Refactored the shader API to make it simpler and more platform agnostic.

engine/opengl/src/shader_opengl.cpp
@@ -13,11 +13,14 @@ static ShaderOpenGL* currentShader = nullptr;
 #pragma warning(disable: 4996)
 #endif
 
-ShaderOpenGL::ShaderOpenGL(String name)
-	: Shader(name)
+ShaderOpenGL::ShaderOpenGL(const ShaderDefinition& definition)
 {
 	id = glCreateProgram();
-	glCheckError();
+	glCheckError();	
+
+	name = definition.name;
+	loadShaders(definition.shaders);
+	compile();
 }
 
 ShaderOpenGL::~ShaderOpenGL()
@@ -44,7 +47,7 @@ void ShaderOpenGL::unbind()
 	glCheckError();
 }
 
-static GLuint loadShader(String src, GLenum type, String name)
+static GLuint loadShader(const Bytes& src, GLenum type, String name)
 {
 	// Create shader
 	GLuint shader = glCreateShader(type);
@@ -53,7 +56,7 @@ static GLuint loadShader(String src, GLenum type, String name)
 	// Load source
 	size_t len = src.size();
 	GLchar* buffer = new GLchar[len + 1];
-	strcpy(buffer, src.c_str());
+	memcpy(buffer, src.data(), src.size());
 	buffer[len] = 0;
 	const char* cbuf = buffer;
 	glShaderSource(shader, 1, &cbuf, nullptr);
@@ -84,22 +87,34 @@ static GLuint loadShader(String src, GLenum type, String name)
 	return shader;
 }
 
+void ShaderOpenGL::loadShaders(const std::map<ShaderType, Bytes>& sources)
+{
+	for (auto& s: sources) {
+		auto type = s.first;
+		int glType = 0;
+		switch (type) {
+		case ShaderType::Vertex:
+			glType = GL_VERTEX_SHADER;
+			break;
+		case ShaderType::Pixel:
+			glType = GL_FRAGMENT_SHADER;
+			break;
+#ifdef WITH_OPENGL
+		case ShaderType::Geometry:
+			glType = GL_GEOMETRY_SHADER;
+			break;
+#endif
+		default:
+			throw Exception("Unsupported shader type: " + toString(type));
+		}
+
+		shaders.push_back(loadShader(s.second, glType, name + "/" + toString(type)));
+	}
+}
+
 void ShaderOpenGL::compile()
 {
 	if (!ready) {
-		// Compile shaders
-		for (size_t i = 0; i<vertexSources.size(); i++) {
-			shaders.push_back(loadShader(vertexSources[i], GL_VERTEX_SHADER, name + "/vertex"));
-		}
-#ifdef WITH_OPENGL
-		for (size_t i = 0; i<geometrySources.size(); i++) {
-			shaders.push_back(loadShader(geometrySources[i], GL_GEOMETRY_SHADER, name + "/geometry"));
-		}
-#endif
-		for (size_t i = 0; i<pixelSources.size(); i++) {
-			shaders.push_back(loadShader(pixelSources[i], GL_FRAGMENT_SHADER, name + "/pixel"));
-		}
-
 		// Create program
 		for (size_t i = 0; i<shaders.size(); i++) {
 			glAttachShader(id, shaders[i]);
@@ -154,21 +169,6 @@ void ShaderOpenGL::destroy()
 		glCheckError();
 		id = 0;
 	}
-}
-
-void ShaderOpenGL::addVertexSource(String src)
-{
-	vertexSources.emplace_back(src);
-}
-
-void ShaderOpenGL::addGeometrySource(String src)
-{
-	geometrySources.emplace_back(src);
-}
-
-void ShaderOpenGL::addPixelSource(String src)
-{
-	pixelSources.emplace_back(src);
 }
 
 unsigned ShaderOpenGL::getUniformLocation(String name)

engine/core/include/halley/core/api/video_api.h
@@ -5,6 +5,7 @@
 
 namespace Halley
 {
+	class ShaderDefinition;
 	class WindowDefinition;
 	class Painter;
 	class Texture;
@@ -25,7 +26,7 @@ namespace Halley
 		virtual const Window& getWindow() const = 0;
 
 		virtual std::unique_ptr<Texture> createTexture(Vector2i size) = 0;
-		virtual std::unique_ptr<Shader> createShader(String name) = 0;
+		virtual std::unique_ptr<Shader> createShader(const ShaderDefinition& definition) = 0;
 		virtual std::unique_ptr<TextureRenderTarget> createRenderTarget() = 0;
 	};
 }

engine/core/include/halley/core/graphics/shader.h
@@ -8,23 +8,37 @@ namespace Halley
 	class MaterialParameterBinding;
 	class MaterialAttribute;
 
+	enum class ShaderType
+	{
+		Vertex,
+		Pixel,
+		Geometry
+	};
+
+	template <>
+	struct EnumNames<ShaderType> {
+		constexpr std::array<const char*, 3> operator()() const {
+			return{{
+				"vertex",
+				"pixel",
+				"geometry"
+			}};
+		}
+	};
+
+	class ShaderDefinition
+	{
+	public:
+		String name;
+		std::map<ShaderType, Bytes> shaders;
+		Vector<MaterialAttribute> vertexAttributes;
+	};
+
 	class Shader
 	{
 	public:
-		explicit Shader(String name);
 		virtual ~Shader() {}
-
 		virtual void bind() = 0;
-		virtual void compile() = 0;
-
-		virtual void addVertexSource(String src) = 0;
-		virtual void addGeometrySource(String src) = 0;
-		virtual void addPixelSource(String src) = 0;
-
-		virtual void setAttributes(const Vector<MaterialAttribute>& attributes) = 0;
 		virtual unsigned int getUniformLocation(String name) = 0;
-		virtual unsigned int getAttributeLocation(String name) = 0;
-	protected:
-		String name;
 	};
 }

engine/core/src/dummy/dummy_video.cpp
@@ -35,9 +35,9 @@ std::unique_ptr<Texture> DummyVideoAPI::createTexture(Vector2i size)
 	return std::make_unique<DummyTexture>(size);
 }
 
-std::unique_ptr<Shader> DummyVideoAPI::createShader(String name)
+std::unique_ptr<Shader> DummyVideoAPI::createShader(const ShaderDefinition&)
 {
-	return std::make_unique<DummyShader>(name);
+	return std::make_unique<DummyShader>();
 }
 
 std::unique_ptr<TextureRenderTarget> DummyVideoAPI::createRenderTarget()
@@ -84,28 +84,9 @@ void DummyTextureRenderTarget::bind() {}
 
 void DummyTextureRenderTarget::unbind() {}
 
-DummyShader::DummyShader(const String& name)
-: Shader(name)
-{}
-
 void DummyShader::bind() {}
 
-void DummyShader::compile() {}
-
-void DummyShader::addVertexSource(String src) {}
-
-void DummyShader::addGeometrySource(String src) {}
-
-void DummyShader::addPixelSource(String src) {}
-
-void DummyShader::setAttributes(const Vector<MaterialAttribute>& attributes) {}
-
 unsigned DummyShader::getUniformLocation(String name)
-{
-	return 0;
-}
-
-unsigned DummyShader::getAttributeLocation(String name)
 {
 	return 0;
 }

engine/core/src/dummy/dummy_video.h
@@ -15,7 +15,7 @@ namespace Halley {
 		void setWindow(WindowDefinition&& windowDescriptor, bool vsync) override;
 		const Window& getWindow() const override;
 		std::unique_ptr<Texture> createTexture(Vector2i size) override;
-		std::unique_ptr<Shader> createShader(String name) override;
+		std::unique_ptr<Shader> createShader(const ShaderDefinition& definition) override;
 		std::unique_ptr<TextureRenderTarget> createRenderTarget() override;
 		void init() override;
 		void deInit() override;
@@ -46,16 +46,8 @@ namespace Halley {
 	class DummyShader : public Shader
 	{
 	public:
-		explicit DummyShader(const String& name);
-
 		void bind() override;
-		void compile() override;
-		void addVertexSource(String src) override;
-		void addGeometrySource(String src) override;
-		void addPixelSource(String src) override;
-		void setAttributes(const Vector<MaterialAttribute>& attributes) override;
 		unsigned getUniformLocation(String name) override;
-		unsigned getAttributeLocation(String name) override;
 	};
 
 	class DummyPainter : public Painter

engine/core/src/graphics/material/material_definition.cpp
@@ -5,9 +5,9 @@
 #include "halley/core/graphics/painter.h"
 #include "halley/core/graphics/material/material_definition.h"
 #include "halley/core/graphics/material/material_parameter.h"
-#include <halley/file_formats/text_file.h>
 #include "halley/file/byte_serializer.h"
 #include "halley/text/string_converter.h"
+#include "halley/file_formats/binary_file.h"
 
 using namespace Halley;
 
@@ -138,17 +138,19 @@ void MaterialPass::deserialize(Deserializer& s)
 
 void MaterialPass::createShader(ResourceLoader& loader, String name, const Vector<MaterialAttribute>& attributes)
 {
-	shader = loader.getAPI().video->createShader(name);
-	shader->setAttributes(attributes);
-	
+	ShaderDefinition definition;
+	definition.name = name;
+	definition.vertexAttributes = attributes;
+
 	if (vertex != "") {
-		shader->addVertexSource(loader.getAPI().getResource<TextFile>("shader/" + vertex)->data);
+		definition.shaders[ShaderType::Vertex] = loader.getAPI().getResource<BinaryFile>("shader/" + vertex)->data;
 	}
 	if (geometry != "") {
-		shader->addGeometrySource(loader.getAPI().getResource<TextFile>("shader/" + geometry)->data);
+		definition.shaders[ShaderType::Geometry] = loader.getAPI().getResource<BinaryFile>("shader/" + geometry)->data;
 	}
 	if (pixel != "") {
-		shader->addPixelSource(loader.getAPI().getResource<TextFile>("shader/" + pixel)->data);
+		definition.shaders[ShaderType::Pixel] = loader.getAPI().getResource<BinaryFile>("shader/" + pixel)->data;
 	}
-	shader->compile();
+
+	shader = loader.getAPI().video->createShader(definition);
 }

engine/core/src/graphics/shader.cpp
@@ -2,7 +2,3 @@
 
 using namespace Halley;
 
-Shader::Shader(String name)
-	: name(name)
-{
-}

engine/core/src/resources/standard_resources.cpp
@@ -8,6 +8,7 @@
 #include "graphics/text/font.h"
 #include "halley/file_formats/config_file.h"
 #include "halley/audio/audio_clip.h"
+#include "halley/file_formats/binary_file.h"
 
 using namespace Halley;
 
@@ -17,6 +18,7 @@ void StandardResources::initialize(Resources& resources)
 	resources.init<SpriteSheet>("spritesheet");
 	resources.init<Texture>("image");
 	resources.init<MaterialDefinition>("material");
+	resources.init<BinaryFile>("");
 	resources.init<TextFile>("");
 	resources.init<Font>("font");
 	resources.init<ConfigFile>("config");

commit a0b75bd1e18890d22c79f9e7b0891068a16890e0

Fix issue with loading OpenGL shaders (introduced back in 3dfc4903c92bab6b1b3b7d040dc2033fcd9d5a41)
Was actually incorrect and shouldn't load anywhere, but nvidia drivers were happy to accept it.

engine/opengl/src/shader_opengl.cpp
@@ -20,6 +20,7 @@ ShaderOpenGL::ShaderOpenGL(const ShaderDefinition& definition)
 	glCheckError();	
 
 	name = definition.name;
+	setAttributes(definition.vertexAttributes);
 	loadShaders(definition.shaders);
 	compile();
 }