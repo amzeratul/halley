6cefc49884f4c685bd6b3de8a559961c0bc606a3
Rodrigo Braz Monteiro <archmagezeratul@gmail.com>
2017-04-17 18:44:03 +0100

    Make OpenGL debug synchronous in debug builds.

--- engine/opengl/src/video_opengl.cpp
+++ engine/opengl/src/video_opengl.cpp
@@ -137,6 +137,10 @@ void VideoOpenGL::setupDebugCallback()
 {
 #ifdef WITH_OPENGL
 	if (glDebugMessageCallback) {
+#ifdef _DEBUG
+		glEnable(GL_DEBUG_OUTPUT);
+		glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
+#endif
 		glDebugMessageCallback([](GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length, const GLchar* message, const void* userParam) {
 			reinterpret_cast<const VideoOpenGL*>(userParam)->onGLDebugMessage(source, type, id, severity, message);
 		}, this);
@@ -222,10 +226,14 @@ void VideoOpenGL::onGLDebugMessage(unsigned int source, unsigned int type, unsig
 #endif
 		std::string str = ss.str();
 
+#ifdef _DEBUG
+		std::cout << ConsoleColour(Console::YELLOW) << str << ConsoleColour() << std::endl;
+#else
 		std::lock_guard<std::mutex> lock(messagesMutex);
 		messagesPending.push_back([str] () {
 			std::cout << ConsoleColour(Console::YELLOW) << str << ConsoleColour() << std::endl;
 		});
+#endif
 	}
 #endif
 }
