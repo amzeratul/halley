04af2af11731575dc7cae308bf211b85ccd19771
Rodrigo Braz Monteiro <archmagezeratul@gmail.com>
2017-04-28 12:37:52 +0100

    Some more higher-order functions

--- shared_assets/lua/halley/halley.lua
+++ shared_assets/lua/halley/halley.lua
@@ -23,13 +23,49 @@ end
 -- Some useful functions
 functional = {}
 
-function functional.any(f, vs)
+function functional.any(vs)
     for i,v in ipairs(vs) do
-        if (f(v)) then
+        if v then
             return true
         end
     end
     return false
+end
+
+function functional.all(vs)
+    for i,v in ipairs(vs) do
+        if not v then
+            return false
+        end
+    end
+    return true
+end
+
+function functional.anyWith(f, vs)
+    for i,v in ipairs(vs) do
+        if f(v) then
+            return true
+        end
+    end
+    return false
+end
+
+function functional.map(f, vs)
+    local result = {}
+    for i,v in ipairs(vs) do
+        table.insert(result, f(v))
+    end
+    return result
+end
+
+function functional.filter(f, vs)
+    local result = {}
+    for i,v in ipairs(vs) do
+        if f(v) then
+            table.insert(result, v)
+        end
+    end
+    return result
 end
 
 return {}
