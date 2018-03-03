080a892619360a8a0d88ec09c3d99730de61adfc
Rodrigo Braz Monteiro <archmagezeratul@gmail.com>
2018-03-03 22:59:24 +0000

    Initialise Media Foundation

--- src/plugins/mf/src/mf_movie_api.cpp
+++ src/plugins/mf/src/mf_movie_api.cpp
@@ -1,12 +1,20 @@
 #include "mf_movie_api.h"
+#include <Mfapi.h>
 using namespace Halley;
+
+#ifdef _MSC_VER
+#pragma comment(lib, "Mfplat.lib")
+#endif
 
 void MFMovieAPI::init()
 {
-	
+	auto hr = MFStartup(MF_VERSION);	
+	if (!SUCCEEDED(hr)) {
+		throw Exception("Unable to initialise MediaFoundation");
+	}
 }
 
 void MFMovieAPI::deInit()
 {
-	
+	MFShutdown();
 }
