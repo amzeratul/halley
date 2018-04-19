87551fe940662002b7da72b269d8c4518ca2f04f
Rodrigo Braz Monteiro <archmagezeratul@gmail.com>
2018-04-19 14:18:16 +0100

    Change alignment of UIImages.

--- src/engine/ui/src/ui/widgets/ui_image.cpp
+++ src/engine/ui/src/ui/widgets/ui_image.cpp
@@ -35,6 +35,7 @@ void UIImage::setSprite(Sprite s)
 	auto c = s.getClip();
 	
 	auto spriteSize = (s.getRawSize().abs() + topLeftBorder + bottomRightBorder) * s.getScale();
+	sprite.setAbsolutePivot(-topLeftBorder);
 	
 	if (c) {
 		setMinSize(Vector2f::min(spriteSize, c.get().getSize()));
