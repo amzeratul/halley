8276df05991b9fa479f719505f012dbf0ae22a76
Rodrigo Braz Monteiro <archmagezeratul@gmail.com>
2018-06-24 21:09:40 +0100

    WIP parsing .ase files.

--- src/tools/tools/src/sprites/aseprite_file.cpp
+++ src/tools/tools/src/sprites/aseprite_file.cpp
@@ -2,9 +2,95 @@
 #include <halley/support/exception.h>
 #include "halley/utils/utils.h"
 #include "halley/support/logger.h"
+#include "halley/file/compression.h"
 using namespace Halley;
 
 // Specs are at https://github.com/aseprite/aseprite/blob/master/docs/ase-file-specs.md
+
+struct AsepriteFileHeader
+{
+	constexpr static size_t size = 128;
+
+    uint32_t fileSize;
+    uint16_t magicNumber;
+    uint16_t frames;
+    uint16_t width;
+    uint16_t height;
+    uint16_t colourDepth;
+    uint32_t flags;
+    uint16_t speed;
+    uint32_t _reserved0;
+    uint32_t _reserved1;
+    uint8_t transparentPaletteEntry;
+    std::array<uint8_t, 3> _reserved2;
+    uint16_t numberOfColours;
+    uint8_t pixelWidth;
+    uint8_t pixelHeight;
+    std::array<uint8_t, 88> _reserved3;
+};
+
+struct AsepriteFileFrameHeader
+{
+	constexpr static size_t size = 16;
+
+    uint32_t dataSize;
+    uint16_t magicNumber;
+    uint16_t chunks;
+    uint16_t duration;
+    std::array<uint8_t, 6> _reserved;
+};
+
+struct AsepriteFileChunkHeader
+{
+	constexpr static size_t size = 6;
+
+    uint32_t dataSize;
+    uint16_t type;
+};
+
+struct AsepriteFileLayerData
+{
+	constexpr static size_t size = 18;
+
+	uint16_t flags;
+	uint16_t layerType;
+	uint16_t childLevel;
+	uint16_t defaultWidth;
+	uint16_t defaultHeight;
+	uint16_t blendMode;
+	uint8_t opacity;
+	std::array<uint8_t, 3> reserved;
+	uint16_t nameLength;
+};
+
+struct AsepriteFileCelData
+{
+	constexpr static size_t size = 16;
+
+	uint16_t layerIndex;
+	int16_t x;
+	int16_t y;
+	uint8_t opacity;
+	uint8_t type_a; // wtf is this misaligned bullshit :(
+	uint8_t type_b;
+	std::array<uint8_t, 7> reserved;
+};
+
+struct AsepriteFileRawCelData
+{
+	constexpr static size_t size = 4;
+
+	uint16_t width;
+	uint16_t height;
+};
+
+struct AsepriteFileLinkedCelData
+{
+	constexpr static size_t size = 2;
+
+	uint16_t framePosition;
+};
+
 
 AsepriteFrame::AsepriteFrame(uint16_t duration)
 	: duration(duration)
@@ -17,20 +103,18 @@ AsepriteFile::AsepriteFile()
 
 void AsepriteFile::load(gsl::span<const gsl::byte> data)
 {
-	Expects(sizeof(AsepriteFileHeader) == 128);
-
 	size_t pos = 0;
 
 	// Read header
 	AsepriteFileHeader fileHeader;
-	if (data.size() < sizeof(fileHeader)) {
+	if (data.size() < AsepriteFileHeader::size) {
 		throw Exception("Invalid Aseprite file (too small)");
 	}
-	memcpy(&fileHeader, data.data(), sizeof(fileHeader));
+	memcpy(&fileHeader, data.data(), AsepriteFileHeader::size);
 	if (fileHeader.magicNumber != 0xA5E0) {
 		throw Exception("Invalid Aseprite file (invalid file magic number)");
 	}
-	pos += sizeof(fileHeader);
+	pos += AsepriteFileHeader::size;
 
 	// Store header data
 	size = Vector2i(int(fileHeader.width), int(fileHeader.height));
@@ -55,20 +139,21 @@ void AsepriteFile::load(gsl::span<const gsl::byte> data)
 	for (int i = 0; i < int(fileHeader.frames); ++i) {
 		const size_t frameStartPos = pos;
 
-		AsepriteFrameHeader frameHeader;
-		memcpy(&frameHeader, data.data() + frameStartPos, sizeof(frameHeader));
+		AsepriteFileFrameHeader frameHeader;
+		memcpy(&frameHeader, data.data() + frameStartPos, AsepriteFileFrameHeader::size);
 		if (frameHeader.magicNumber != 0xF1FA) {
 			throw Exception("Invalid Aseprite file (invalid frame magic number)");
 		}
 		addFrame(frameHeader.duration);
+		pos += AsepriteFileFrameHeader::size;
 
 		// Read each chunk
 		for (int j = 0; j < int(frameHeader.chunks); ++j) {
 			const size_t chunkStartPos = pos;
 
-			AsepriteChunkHeader chunkHeader;
-			memcpy(&chunkHeader, data.data() + chunkStartPos, sizeof(chunkHeader));
-			addChunk(chunkHeader.type, data.subspan(chunkStartPos + sizeof(chunkHeader), chunkHeader.dataSize - sizeof(chunkHeader)));
+			AsepriteFileChunkHeader chunkHeader;
+			memcpy(&chunkHeader, data.data() + chunkStartPos, AsepriteFileChunkHeader::size);
+			addChunk(chunkHeader.type, data.subspan(chunkStartPos + AsepriteFileChunkHeader::size, chunkHeader.dataSize - AsepriteFileChunkHeader::size));
 
 			pos = chunkStartPos + chunkHeader.dataSize;
 		}
@@ -130,23 +215,12 @@ void AsepriteFile::addChunk(uint16_t chunkType, gsl::span<const std::byte> data)
 
 void AsepriteFile::addLayerChunk(gsl::span<const std::byte> span)
 {
-	struct LayerData
-	{
-		uint16_t flags;
-		uint16_t layerType;
-		uint16_t childLevel;
-		uint16_t defaultWidth;
-		uint16_t defaultHeight;
-		uint16_t blendMode;
-		uint8_t opacity;
-		std::array<uint8_t, 3> reserved;
-	};
-
-	LayerData data;
-	if (span.size() < sizeof(data)) {
+	AsepriteFileLayerData data;
+	if (span.size() < AsepriteFileLayerData::size) {
 		throw Exception("Invalid layer data");
 	}
-	memcpy(&data, span.data(), sizeof(data));
+	memcpy(&data, span.data(), AsepriteFileLayerData::size);
+	span = span.subspan(AsepriteFileLayerData::size);
 
 	layers.push_back(AsepriteLayer());
 	auto& layer = layers.back();
@@ -161,12 +235,64 @@ void AsepriteFile::addLayerChunk(gsl::span<const std::byte> span)
 	layer.layerGroupDisplaysCollapsed = (data.flags & 32) != 0;
 	layer.referenceLayer = (data.flags & 64) != 0;
 	layer.opacity = (flags & 1) != 0 ? data.opacity : 255;
-	layer.layerName = reinterpret_cast<const char*>(span.data() + sizeof(data));
+
+	layer.layerName = String(reinterpret_cast<const char*>(span.data()), data.nameLength);
 }
 
 void AsepriteFile::addCelChunk(gsl::span<const std::byte> span)
 {
-	// TODO
+	AsepriteCel cel;
+
+	AsepriteFileCelData baseData;
+	if (span.size() < AsepriteFileCelData::size) {
+		throw Exception("Invalid cel data");
+	}
+	memcpy(&baseData, span.data(), AsepriteFileCelData::size);
+	span = span.subspan(AsepriteFileCelData::size);
+
+	cel.opacity = baseData.opacity;
+	cel.pos = Vector2i(int(baseData.x), int(baseData.y));
+	cel.layer = baseData.layerIndex;
+
+	int type = int(baseData.type_a);
+	if (type == 0 || type == 2) {
+		// Raw or compressed
+		AsepriteFileRawCelData header;
+		if (span.size() < AsepriteFileRawCelData::size) {
+			throw Exception("Invalid cel data");
+		}
+		memcpy(&header, span.data(), AsepriteFileRawCelData::size);
+		span = span.subspan(AsepriteFileRawCelData::size);
+
+		cel.size = Vector2i(int(header.width), int(header.height));
+
+		// Read pixels
+		if (type == 0) {
+			// Raw
+			cel.rawData.resize(cel.size.x * cel.size.y);
+			if (span.size() < cel.rawData.size()) {
+				throw Exception("Invalid cel data");
+			}
+			memcpy(cel.rawData.data(), span.data(), cel.rawData.size());
+		} else if (type == 2) {
+			// ZLIB compressed
+			cel.rawData = Compression::inflate(span);
+		}
+	} else if (type == 1) {
+		// Linked
+		AsepriteFileLinkedCelData header;
+		if (span.size() < AsepriteFileLinkedCelData::size) {
+			throw Exception("Invalid cel data");
+		}
+		memcpy(&header, span.data(), AsepriteFileLinkedCelData::size);
+
+		cel.linked = true;
+		cel.linkedFrame = header.framePosition;
+	} else {
+		throw Exception("Invalid cel type: " + toString(type));
+	}
+
+	frames.back().cels.push_back(std::move(cel));
 }
 
 void AsepriteFile::addCelExtraChunk(gsl::span<const std::byte> span)
--- src/tools/tools/src/sprites/aseprite_file.h
+++ src/tools/tools/src/sprites/aseprite_file.h
@@ -6,46 +6,24 @@
 #include "halley/maths/vector2.h"
 
 namespace Halley {
-    struct AsepriteFileHeader
-    {
-        uint32_t fileSize;
-        uint16_t magicNumber;
-        uint16_t frames;
-        uint16_t width;
-        uint16_t height;
-        uint16_t colourDepth;
-        uint32_t flags;
-        uint16_t speed;
-        uint32_t _reserved0;
-        uint32_t _reserved1;
-        uint8_t transparentPaletteEntry;
-        std::array<uint8_t, 3> _reserved2;
-        uint16_t numberOfColours;
-        uint8_t pixelWidth;
-        uint8_t pixelHeight;
-        std::array<uint8_t, 88> _reserved3;
-    };
-
-    struct AsepriteFrameHeader
-    {
-        uint32_t dataSize;
-        uint16_t magicNumber;
-        uint16_t chunks;
-        uint16_t duration;
-        std::array<uint8_t, 6> _reserved;
-    };
-
-    struct AsepriteChunkHeader
-    {
-        uint32_t dataSize;
-        uint16_t type;
-    };
-
 	enum class AsepriteDepth
 	{
 		RGBA32,
 		Greyscale16,
 		Indexed8
+	};
+
+	struct AsepriteCel
+	{
+	public:
+		Vector2i pos;
+		Vector2i size;
+		uint16_t layer = 0;
+		uint16_t linkedFrame = 0;
+		uint8_t opacity = 255;
+		bool linked = false;
+
+		Bytes rawData;
 	};
 
 	struct AsepriteFrame
@@ -54,6 +32,7 @@ namespace Halley {
 		AsepriteFrame(uint16_t duration);
 
 		uint16_t duration;
+		std::vector<AsepriteCel> cels;
 	};
 
 	enum class AsepriteLayerType
