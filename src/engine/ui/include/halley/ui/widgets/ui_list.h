2185788b305f5f60073f3ca7db96605890e8ce01
caryn <caryn@chucklefish.org>
2018-07-13 16:01:46 +0100

    able to override the style of list items

--- src/engine/ui/include/halley/ui/widgets/ui_list.h
+++ src/engine/ui/include/halley/ui/widgets/ui_list.h
@@ -23,8 +23,8 @@ namespace Halley {
 		size_t getCount() const;
 
 		void addTextItem(const String& id, const LocalisedString& label, float maxWidth = -1, bool centre = false);
-		void addItem(const String& id, std::shared_ptr<UIWidget> widget, float proportion = 0, Vector4f border = {}, int fillFlags = UISizerFillFlags::Fill);
-		void addItem(const String& id, std::shared_ptr<UISizer> sizer, float proportion = 0, Vector4f border = {}, int fillFlags = UISizerFillFlags::Fill);
+		void addItem(const String& id, std::shared_ptr<UIWidget> widget, float proportion = 0, Vector4f border = {}, int fillFlags = UISizerFillFlags::Fill, std::shared_ptr<UIStyle> styleOverride = nullptr);
+		void addItem(const String& id, std::shared_ptr<UISizer> sizer, float proportion = 0, Vector4f border = {}, int fillFlags = UISizerFillFlags::Fill, std::shared_ptr<UIStyle> styleOverride = nullptr);
 		void clear();
 
 		void setItemEnabled(const String& id, bool enabled);
