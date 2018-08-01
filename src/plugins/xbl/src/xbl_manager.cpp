c0af2dbddc18ba061b68a9da12a638fc44d68f04
Rodrigo Braz Monteiro <archmagezeratul@gmail.com>
2018-08-01 17:21:19 +0100

    WIP WinRT save data.

--- src/plugins/xbl/src/xbl_manager.cpp
+++ src/plugins/xbl/src/xbl_manager.cpp
@@ -1,13 +1,32 @@
 #include "xbl_manager.h"
-#include "xsapi/services.h"
 #include "halley/support/logger.h"
 #include "halley/text/halleystring.h"
-#include <Windows.UI.Core.h>
-
+#include <winrt/Windows.UI.Core.h>
+#include <winrt/Windows.Gaming.XboxLive.Storage.h>
 #include "xsapi/services.h"
+#include "halley/concurrency/concurrent.h"
 
-using namespace xbox::services::system;
+#include <winrt/Windows.System.UserProfile.h>
+
 using namespace Halley;
+
+template <typename T>
+T from_cx(Platform::Object^ from)
+{
+    T to{ nullptr };
+
+    winrt::check_hresult(reinterpret_cast<::IUnknown*>(from)
+        ->QueryInterface(winrt::guid_of<T>(),
+            reinterpret_cast<void**>(winrt::put_abi(to))));
+
+    return to;
+}
+
+template <typename T>
+T^ to_cx(winrt::Windows::Foundation::IUnknown const& from)
+{
+    return safe_cast<T^>(reinterpret_cast<Platform::Object^>(winrt::get_abi(from)));
+}
 
 XBLManager::XBLManager()
 {
@@ -21,34 +40,45 @@ XBLManager::~XBLManager()
 
 void XBLManager::init()
 {
-	xboxUser = std::make_shared<xbox_live_user>(nullptr);
-	auto dispatcher = ::Windows::UI::Core::CoreWindow::GetForCurrentThread()->Dispatcher;
+	signIn();
+}
 
-	xboxUser->signin_silently(dispatcher).then([=] (xbox::services::xbox_live_result<sign_in_result> result)
+void XBLManager::deInit()
+{
+}
+
+void XBLManager::signIn()
+{
+	using namespace xbox::services::system;
+	xboxUser = std::make_shared<xbox_live_user>(nullptr);
+	auto dispatcher = to_cx<Platform::Object>(winrt::Windows::UI::Core::CoreWindow::GetForCurrentThread().Dispatcher());
+
+	xboxUser->signin_silently(dispatcher).then([=] (xbox::services::xbox_live_result<sign_in_result> result) -> winrt::Windows::Foundation::IAsyncAction
 	{
 		if (result.err()) {
 			Logger::logError(String("Error signing in to Xbox Live: ") + result.err_message());
 		} else {
 			bool loggedIn = false;
 			switch (result.payload().status()) {
-			case sign_in_status::success:
+			case success:
 				loggedIn = true;
 				break;
 
-			case sign_in_status::user_interaction_required:
-				xboxUser->signin(dispatcher).then([&](xbox::services::xbox_live_result<xbox::services::system::sign_in_result> loudResult)
+			case user_interaction_required:
+				xboxUser->signin(dispatcher).then([&](xbox::services::xbox_live_result<sign_in_result> loudResult)
                 {
                     if (!loudResult.err()) {
                         auto resPayload = loudResult.payload();
                         switch (resPayload.status()) {
-                        case xbox::services::system::sign_in_status::success:
+                        case success:
                             loggedIn = true;
                             break;
-                        case xbox::services::system::sign_in_status::user_cancel:
+                        case user_cancel:
                             break;
                         }
                     }
                 }, concurrency::task_continuation_context::use_current());
+				break;
 			}
 
 			if (loggedIn) {
@@ -61,9 +91,23 @@ void XBLManager::init()
 				});
 			}
 		}
+
+		co_await getConnectedStorage();
+		Logger::logInfo("Storage thread done.");
 	});
 }
 
-void XBLManager::deInit()
+winrt::Windows::Foundation::IAsyncAction XBLManager::getConnectedStorage()
 {
+	using namespace winrt::Windows::Gaming::XboxLive::Storage;
+	
+	auto windowsUser = co_await winrt::Windows::System::User::FindAllAsync();
+
+	//auto user = from_cx<winrt::Windows::System::User>(windowsUser.First());
+	GameSaveProviderGetResult result = co_await GameSaveProvider::GetForUserAsync(*windowsUser.First(), xboxLiveContext->application_config()->scid());
+
+	if (result.Status() == GameSaveErrorStatus::Ok) {
+		gameSaveProvider = result.Value();
+		Logger::logInfo("Got game save provider");
+	}
 }
--- src/plugins/xbl/src/xbl_manager.h
+++ src/plugins/xbl/src/xbl_manager.h
@@ -1,13 +1,16 @@
 #pragma once
 
 #include <memory>
+#include <winrt/base.h>
+#include <winrt/Windows.Gaming.XboxLive.Storage.h>
+#include "halley/data_structures/maybe.h"
 
 namespace xbox {
 	namespace services {
+		class xbox_live_context;
 		namespace system {
 			class xbox_live_user;
 		}
-		class xbox_live_context;
 	}
 }
 
@@ -23,5 +26,9 @@ namespace Halley {
 	private:
 		std::shared_ptr<xbox::services::system::xbox_live_user> xboxUser;
 		std::shared_ptr<xbox::services::xbox_live_context> xboxLiveContext;
+		Maybe<winrt::Windows::Gaming::XboxLive::Storage::GameSaveProvider> gameSaveProvider;
+
+		void signIn();
+		winrt::Windows::Foundation::IAsyncAction getConnectedStorage();
 	};
 }
