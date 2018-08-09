20547658e45346754883e841972dca26d0039f57
Rodrigo Braz Monteiro <archmagezeratul@gmail.com>
2018-08-09 16:42:06 +0100

    WinRT keyboard partially working.

--- src/plugins/winrt/src/winrt_keyboard.cpp
+++ src/plugins/winrt/src/winrt_keyboard.cpp
@@ -1,16 +1,147 @@
 #include "winrt_keyboard.h"
-#include <winrt/Windows.UI.Text.h>
+#include <halley/core/input/input_keys.h>
 
 using namespace Halley;
 
+using namespace winrt::Windows::UI::Core;
+using namespace winrt::Windows::System;
 
-std::unique_ptr<ITextInputCapture> WinRTKeyboard::makeTextInputCapture()
+Maybe<int> WinRTKeyboard::getHalleyKey(VirtualKey virtualKey)
 {
-	return std::make_unique<WinRTTextInputCapture>();
+	int code = int(virtualKey);
+	if (code < 0 || code >= int(keyMapping.size())) {
+		return {};
+	}
+
+	int value = keyMapping[code];
+	if (value == -1) {
+		return {};
+	} else {
+		return value;
+	}
+}
+
+WinRTTextInputCapture::WinRTTextInputCapture(WinRTKeyboard& parent)
+	: parent(parent)
+{
+	parent.addCapture(this);
+}
+
+WinRTTextInputCapture::~WinRTTextInputCapture()
+{
+	parent.removeCapture(this);
+}
+
+WinRTKeyboard::WinRTKeyboard()
+	: InputKeyboard(Keys::Last)
+{
+	initMapping();
+
+	window = CoreWindow::GetForCurrentThread();
+	
+	keyDownEvent = window->KeyDown([=] (CoreWindow window, const KeyEventArgs& args)
+	{
+		auto key = getHalleyKey(args.VirtualKey());
+		if (key) {
+			onButtonPressed(key.get());
+		}
+		args.Handled(key.is_initialized());
+	});
+
+	keyUpEvent = window->KeyDown([=] (CoreWindow window, const KeyEventArgs& args)
+	{
+		auto key = getHalleyKey(args.VirtualKey());
+		if (key) {
+			onButtonReleased(key.get());
+		}
+		args.Handled(key.is_initialized());
+	});
+}
+
+WinRTKeyboard::~WinRTKeyboard()
+{
+	window->KeyDown(keyDownEvent);
+	window->KeyUp(keyUpEvent);
 }
 
 void WinRTKeyboard::update()
 {
+}
+
+void WinRTKeyboard::addCapture(WinRTTextInputCapture* capture)
+{
+	captures.insert(capture);
+}
+
+void WinRTKeyboard::removeCapture(WinRTTextInputCapture* capture)
+{
+	captures.erase(capture);
+}
+
+void WinRTKeyboard::onTextControlCharacterGenerated(TextControlCharacter chr)
+{
+	for (auto& c: captures) {
+		c->onControlCharacter(chr);
+	}
+}
+
+void WinRTKeyboard::initMapping()
+{
+	keyMapping.resize(256, -1);
+
+	auto set = [&] (VirtualKey key, int value)
+	{
+		keyMapping[int(key)] = value;
+	};
+
+	set(VirtualKey::Enter, Keys::Enter);
+	set(VirtualKey::Escape, Keys::Esc);
+	set(VirtualKey::Tab, Keys::Tab);
+	set(VirtualKey::Space, Keys::Space);
+	set(VirtualKey::Delete, Keys::Delete);
+	set(VirtualKey::Back, Keys::Backspace);
+	
+	set(VirtualKey::Up, Keys::Up);
+	set(VirtualKey::Down, Keys::Down);
+	set(VirtualKey::Left, Keys::Left);
+	set(VirtualKey::Right, Keys::Right);
+	set(VirtualKey::Home, Keys::Home);
+	set(VirtualKey::End, Keys::End);
+	set(VirtualKey::PageUp, Keys::PageUp);
+	set(VirtualKey::PageDown, Keys::PageDown);
+
+	set(VirtualKey::LeftControl, Keys::LCtrl);
+	set(VirtualKey::RightControl, Keys::RCtrl);
+	set(VirtualKey::LeftShift, Keys::LShift);
+	set(VirtualKey::RightShift, Keys::RShift);
+	set(VirtualKey::LeftMenu, Keys::LAlt);
+	set(VirtualKey::RightMenu, Keys::LAlt);
+
+	set(VirtualKey::F1, Keys::F1);
+	set(VirtualKey::F2, Keys::F2);
+	set(VirtualKey::F3, Keys::F3);
+	set(VirtualKey::F4, Keys::F4);
+	set(VirtualKey::F5, Keys::F5);
+	set(VirtualKey::F6, Keys::F6);
+	set(VirtualKey::F7, Keys::F7);
+	set(VirtualKey::F8, Keys::F8);
+	set(VirtualKey::F9, Keys::F9);
+	set(VirtualKey::F10, Keys::F10);
+	set(VirtualKey::F11, Keys::F11);
+	set(VirtualKey::F12, Keys::F12);
+
+	for (int i = 0; i < 26; ++i) {
+		set(VirtualKey(int(VirtualKey::A) + i), Keys::A + i);
+	}
+
+	for (int i = 0; i < 10; ++i) {
+		set(VirtualKey(int(VirtualKey::Number0) + i), Keys::Num0 + i);
+	}
+}
+
+std::unique_ptr<ITextInputCapture> WinRTKeyboard::makeTextInputCapture()
+{
+	return std::make_unique<WinRTTextInputCapture>(*this);
 }
 
 void WinRTTextInputCapture::open(TextInputData& input_, SoftwareKeyboardData softKeyboardData)
@@ -75,4 +206,31 @@ bool WinRTTextInputCapture::isOpen() const
 
 void WinRTTextInputCapture::update()
 {
+}
+
+void WinRTTextInputCapture::onControlCharacter(TextControlCharacter chr)
+{
+	using namespace winrt::Windows::UI::Text::Core;
+
+	auto oldText = input->getText();
+
+	CoreTextRange fullRange;
+	fullRange.StartCaretPosition = 0;
+	fullRange.EndCaretPosition = int(input->getText().size());
+
+	CoreTextRange oldSel;
+	oldSel.StartCaretPosition = input->getSelection().start;
+	oldSel.EndCaretPosition = input->getSelection().end;
+
+	input->onControlCharacter(chr, {});
+
+	CoreTextRange newSel;
+	newSel.StartCaretPosition = input->getSelection().start;
+	newSel.EndCaretPosition = input->getSelection().end;
+
+	if (oldText != input->getText()) {
+		editContext->NotifyTextChanged(fullRange, int32_t(input->getText().size()), newSel);
+	} else if (oldSel != newSel) {
+		editContext->NotifySelectionChanged(newSel);
+	}
 }
--- src/plugins/winrt/src/winrt_keyboard.h
+++ src/plugins/winrt/src/winrt_keyboard.h
@@ -2,24 +2,49 @@
 
 #include "halley/core/input/input_keyboard.h"
 #include <winrt/Windows.UI.Text.Core.h>
+#include <winrt/Windows.UI.Core.h>
+#include <winrt/Windows.System.h>
+#include <set>
 
 namespace Halley {
+	class WinRTTextInputCapture;
+
 	class WinRTKeyboard : public InputKeyboard {
 	public:
+		WinRTKeyboard();
+		~WinRTKeyboard();
 		void update();
+		
+		void addCapture(WinRTTextInputCapture* capture);
+		void removeCapture(WinRTTextInputCapture* capture);
 
 	protected:
+		Maybe<winrt::Windows::UI::Core::CoreWindow> window;
+		winrt::event_token keyDownEvent;
+		winrt::event_token keyUpEvent;
+
+		std::vector<int> keyMapping;
+		std::set<WinRTTextInputCapture*> captures;
+
+		void initMapping();
 		std::unique_ptr<ITextInputCapture> makeTextInputCapture() override;
+		Maybe<int> getHalleyKey(winrt::Windows::System::VirtualKey virtualKey);
+		void onTextControlCharacterGenerated(TextControlCharacter chr);
 	};
 
 	class WinRTTextInputCapture : public ITextInputCapture {
 	public:
+		WinRTTextInputCapture(WinRTKeyboard& parent);
+		~WinRTTextInputCapture();
+
 		void open(TextInputData& input, SoftwareKeyboardData softKeyboardData) override;
 		void close() override;
 		bool isOpen() const override;
 		void update() override;
+		void onControlCharacter(TextControlCharacter chr);
 
 	private:
+		WinRTKeyboard & parent;
 		TextInputData* input = nullptr;
 		Maybe<winrt::Windows::UI::Text::Core::CoreTextServicesManager> servicesManager;
 		Maybe<winrt::Windows::UI::Text::Core::CoreTextEditContext> editContext;
