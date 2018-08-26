80431068baa4261fb9bd4ef200ad1747b0e73924
Rodrigo Braz Monteiro <archmagezeratul@gmail.com>
2018-08-26 18:38:37 +0100

    Fix last commit.

--- src/engine/utils/include/halley/file/compression.h
+++ src/engine/utils/include/halley/file/compression.h
@@ -10,7 +10,7 @@ namespace Halley {
 		static Bytes deflate(gsl::span<const gsl::byte> bytes);
 		static Bytes inflate(const Bytes& bytes, size_t maxSize = std::numeric_limits<size_t>::max());
 		static Bytes inflate(gsl::span<const gsl::byte> bytes, size_t maxSize = std::numeric_limits<size_t>::max());
-		static std::shared_ptr<const char> inflateToSharedPtr(gsl::span<const gsl::byte> bytes, size_t& outSize, size_t maxSize);
-		static unsigned char* inflateRaw(gsl::span<const gsl::byte> bytes, size_t& outSize, size_t maxSize = std::numeric_limits<size_t>::max());
+		static std::shared_ptr<const char> inflateToSharedPtr(gsl::span<const gsl::byte> bytes, size_t& outSize, size_t maxSize = std::numeric_limits<size_t>::max());
+		static unsigned char* inflateRaw(gsl::span<const gsl::byte> bytes, size_t& outSize, size_t maxSize);
 	};
 }
