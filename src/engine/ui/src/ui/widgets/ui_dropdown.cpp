dcf5a0d3eada5972aef7c32efae691adc5873876
ckrakauer <caryn@chucklefish.org>
2018-12-19 16:21:56 +0000

    dropdowns will be smaller if there's less room between it and the bottom of the UI rect

--- src/engine/ui/src/ui/widgets/ui_dropdown.cpp
+++ src/engine/ui/src/ui/widgets/ui_dropdown.cpp
@@ -208,7 +208,11 @@ void UIDropdown::open()
 		dropdownList->setInputButtons(inputButtons);
 		dropdownList->setFocused(true);
 
-		scrollPane = std::make_shared<UIScrollPane>(Vector2f(0, 80), UISizer(UISizerType::Vertical, 0));
+		auto standardHeight = 80.0f;
+		auto distanceFromBottom = getRoot()->getRect().getBottom() - getRect().getBottom() - 5.0f;
+		auto height = distanceFromBottom < 0 ? standardHeight : std::min(standardHeight, distanceFromBottom);
+
+		scrollPane = std::make_shared<UIScrollPane>(Vector2f(0, height), UISizer(UISizerType::Vertical, 0));
 		scrollPane->add(dropdownList);
 
 		auto scrollBar = std::make_shared<UIScrollBar>(UIScrollDirection::Vertical, scrollbarStyle);
