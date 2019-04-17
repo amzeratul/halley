b8dbafe1fb43598ea99525e95e97cdd518e2a14a
Rodrigo Braz Monteiro <archmagezeratul@gmail.com>
2019-04-17 18:17:43 +0100

    eh nm we're using linear space

--- shared_assets/shader/line_solid.pixel.hlsl
+++ shared_assets/shader/line_solid.pixel.hlsl
@@ -10,10 +10,7 @@ float4 main(VOut input) : SV_TARGET {
     float edge = input.width * 0.5;
     float invFeather = 2.0 / ddx(input.worldPos.x);
     float v = clamp((edge - abs(input.vertPos)) * invFeather, -1, 1) * 0.5 + 0.5;
-
-    // Generate gamma-correct alpha
-    // Should be pow(v, 1.0 / 2.2), but sqrt() is faster and the difference is very small
-    float a = input.colour.a * sqrt(v);
+    float a = input.colour.a * v;
 
 	return float4(input.colour.rgb * a, a);
 }
