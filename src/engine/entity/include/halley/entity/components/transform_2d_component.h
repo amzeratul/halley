3d98a8ef814ec70e8361d4ae639731ba942c3388
Rodrigo Braz Monteiro <archmagezeratul@gmail.com>
2020-03-03 18:00:07 +0000

    Fix visibility of onAddedToEntity() (Different compilers seem to disagree on behaviour here)

--- src/engine/entity/include/halley/entity/components/transform_2d_component.h
+++ src/engine/entity/include/halley/entity/components/transform_2d_component.h
@@ -60,6 +60,8 @@ public:
 	void addChild(Transform2DComponent& childTransform, bool keepLocalPosition = false);
 	void detachChildren(Halley::World& world);
 
+	void onAddedToEntity(Halley::EntityRef& entity);
+
 private:
 	friend class Halley::EntityRef;
 
@@ -67,6 +69,4 @@ private:
 	Halley::EntityId myId;
 	Halley::EntityId parentId;
 	std::vector<Halley::EntityId> childIds;
-
-	void onAddedToEntity(Halley::EntityRef& entity);
 };
