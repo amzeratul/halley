2e98b7f7914c8fd04c33a69eb38d991284820207
Rodrigo Braz Monteiro <archmagezeratul@gmail.com>
2020-03-09 16:37:57 +0000

    Alpha cutout.

---
+++ shared_assets/shader/sprite_cutout.pixel.hlsl
@@ -0,0 +1,12 @@
+#include "halley/sprite_attribute.hlsl"
+
+Texture2D tex0 : register(t0);
+SamplerState sampler0 : register(s0);
+
+float4 main(VOut input) : SV_TARGET {
+    float4 col = tex0.Sample(sampler0, input.texCoord0.xy);
+    if (col.a < 0.5) {
+        discard;
+    }
+    return col * input.colour + input.colourAdd;
+}
