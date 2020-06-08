4c6c05aaaf698d6a11152e04c19c2e7e458291d6
Rodrigo Braz Monteiro <archmagezeratul@gmail.com>
2020-06-08 17:41:59 +0100

    Bump DLL API version.

--- src/engine/core/include/halley/core/entry/entry_point.h
+++ src/engine/core/include/halley/core/entry/entry_point.h
@@ -15,7 +15,7 @@ namespace Halley
 	class Game;
 	class Core;
 
-	constexpr static uint32_t HALLEY_DLL_API_VERSION = 4;
+	constexpr static uint32_t HALLEY_DLL_API_VERSION = 5;
 	
 	class IHalleyEntryPoint
 	{
