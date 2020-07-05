4b91f9593b670fc9e7280ab86561c45ff906e166
Rodrigo Braz Monteiro <archmagezeratul@gmail.com>
2020-07-05 17:09:10 +0100

    Setup UI in Halley Launcher

--- src/tools/launcher/src/launcher_stage.cpp
+++ src/tools/launcher/src/launcher_stage.cpp
@@ -3,15 +3,53 @@ using namespace Halley;
 
 void LauncherStage::init()
 {
-	
+	makeUI();
 }
 
-void LauncherStage::onVariableUpdate(Time)
+void LauncherStage::onVariableUpdate(Time time)
 {
-	
+	updateUI(time);
 }
 
-void LauncherStage::onRender(RenderContext&) const
+void LauncherStage::onRender(RenderContext& context) const
 {
-	
+	ui->render(context);
+
+	context.bind([&](Painter& painter)
+	{
+		painter.clear(Colour4f()); // Needed for depth/stencil
+		auto view = Rect4f(painter.getViewPort());
+
+		// UI
+		SpritePainter spritePainter;
+		spritePainter.start();
+		ui->draw(spritePainter, 1, 0);
+		spritePainter.draw(1, painter);
+	});
+}
+
+void LauncherStage::makeUI()
+{
+	auto styleSheet = std::make_shared<UIStyleSheet>(getResources());
+	for (auto& style: getResources().enumerate<ConfigFile>()) {
+		if (style.startsWith("ui_style/")) {
+			styleSheet->load(*getResources().get<ConfigFile>(style));
+		}
+	}
+
+	uiFactory = std::make_unique<UIFactory>(getAPI(), getResources(), i18n, styleSheet);
+	ui = std::make_unique<UIRoot>(getAPI());
+	ui->makeToolTip(uiFactory->getStyle("tooltip"));
+
+	topLevelUI = uiFactory->makeUI("ui/load_project");
+	topLevelUI->setAnchor(UIAnchor());
+	ui->addChild(topLevelUI);
+}
+
+void LauncherStage::updateUI(Time time)
+{
+	const auto kb = getInputAPI().getKeyboard();
+	const auto size = getVideoAPI().getWindow().getDefinition().getSize();
+	ui->setRect(Rect4f(Vector2f(), Vector2f(size)));
+	ui->update(time, UIInputType::Mouse, getInputAPI().getMouse(), kb);
 }
--- src/tools/launcher/src/launcher_stage.h
+++ src/tools/launcher/src/launcher_stage.h
@@ -9,5 +9,14 @@ namespace Halley {
 
 		void onVariableUpdate(Time) override;
 		void onRender(RenderContext&) const override;
+
+	private:
+		I18N i18n;
+		std::unique_ptr<UIFactory> uiFactory;
+		std::unique_ptr<UIRoot> ui;
+		std::shared_ptr<UIWidget> topLevelUI;
+
+		void makeUI();
+		void updateUI(Time time);
 	};
 }
