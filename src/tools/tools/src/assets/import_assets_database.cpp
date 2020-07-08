84be3c4e8b385fa3973c5ab7e4c73104a4d07364
Rodrigo Braz Monteiro <archmagezeratul@gmail.com>
2020-07-08 17:56:12 +0100

    Bump asset version (nice).

--- src/tools/tools/src/assets/import_assets_database.cpp
+++ src/tools/tools/src/assets/import_assets_database.cpp
@@ -4,7 +4,7 @@
 #include "halley/resources/resource_data.h"
 #include "halley/tools/file/filesystem.h"
 
-constexpr static int currentAssetVersion = 68;
+constexpr static int currentAssetVersion = 69;
 
 using namespace Halley;
 
