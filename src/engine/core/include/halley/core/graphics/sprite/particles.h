b911ad1567228b41eaba626ab13228f419d811e6
Rodrigo Braz Monteiro <archmagezeratul@gmail.com>
2020-07-30 17:04:21 +0100

    WIP particle system support. A lot of hardcoded stuff atm for testing.

--- src/engine/core/include/halley/core/graphics/sprite/particles.h
+++ src/engine/core/include/halley/core/graphics/sprite/particles.h
@@ -4,8 +4,24 @@
 #include <halley/time/halleytime.h>
 
 namespace Halley {
+	class Random;
+	
 	class Particles {
+		struct Particle {
+			Vector2f pos;
+			Vector2f vel;
+			Angle1f angle;
+			float ttl = 1;
+			bool alive = true;
+		};
+		
 	public:
+		Particles();
+		Particles(const ConfigNode& node, Resources& resources);
+
+		ConfigNode toConfigNode() const;
+
+		void setPosition(Vector2f pos);
 		void update(Time t);
 		
 		[[nodiscard]] gsl::span<Sprite> getSprites();
@@ -14,12 +30,33 @@ namespace Halley {
 		[[nodiscard]] int getLayer() const;
 
 	private:
+		Random* rng;
 		std::shared_ptr<Material> material;
 
 		std::vector<Sprite> sprites;
-
-		size_t nSprites = 0;
+		std::vector<Particle> particles;
+		size_t nParticlesAlive = 0;
+		
 		int mask = 1;
 		int layer = 0;
+		float spawnRate = 10;
+		float pendingSpawn = 0;
+
+		Sprite baseSprite;
+		Vector2f position;
+
+		void spawn(size_t n);
+		void initializeParticle(size_t index);
+		void updateParticles(float t);
+
+		Vector2f getSpawnPosition() const;
+	};
+
+	class Resources;
+	template<>
+	class ConfigNodeSerializer<Particles> {
+	public:
+		ConfigNode serialize(const Particles& particles, ConfigNodeSerializationContext& context);
+		Particles deserialize(ConfigNodeSerializationContext& context, const ConfigNode& node);
 	};
 }
--- src/engine/core/src/graphics/sprite/particles.cpp
+++ src/engine/core/src/graphics/sprite/particles.cpp
@@ -1,20 +1,69 @@
 #include "graphics/sprite/particles.h"
 
+#include "halley/maths/random.h"
+
 using namespace Halley;
+
+Particles::Particles()
+	: rng(&Random::getGlobal())
+{
+}
+
+Particles::Particles(const ConfigNode& node, Resources& resources)
+	: rng(&Random::getGlobal())
+{
+	baseSprite.setImage(resources, "whitebox.png").setSize(Vector2f(10, 0.5f)).setColour(Colour4f(1, 1, 1, 0.5f));
+	spawnRate = 500;
+}
+
+ConfigNode Particles::toConfigNode() const
+{
+	ConfigNode::MapType result;
+	// TODO
+	return result;
+}
+
+void Particles::setPosition(Vector2f pos)
+{
+	position = pos;
+}
 
 void Particles::update(Time t)
 {
-	
+	pendingSpawn += static_cast<float>(t * spawnRate);
+	const int toSpawn = static_cast<int>(floor(pendingSpawn));
+	pendingSpawn = pendingSpawn - static_cast<float>(toSpawn);
+
+	// Spawn new particles
+	spawn(static_cast<size_t>(toSpawn));
+
+	// Update particles
+	updateParticles(static_cast<float>(t));
+
+	// Remove dead particles
+	for (size_t i = 0; i < nParticlesAlive; ) {
+		if (!particles[i].alive) {
+			if (i != nParticlesAlive - 1) {
+				// Swap with last particle that's alive
+				std::swap(particles[i], particles[nParticlesAlive - 1]);
+				std::swap(sprites[i], sprites[nParticlesAlive - 1]);
+			}
+			--nParticlesAlive;
+			// Don't increment i here, since i is now a new particle that's still alive
+		} else {
+			++i;
+		}
+	}
 }
 
 gsl::span<Sprite> Particles::getSprites()
 {
-	return gsl::span<Sprite>(sprites).subspan(0, nSprites);
+	return gsl::span<Sprite>(sprites).subspan(0, nParticlesAlive);
 }
 
 gsl::span<const Sprite> Particles::getSprites() const
 {
-	return gsl::span<const Sprite>(sprites).subspan(0, nSprites);
+	return gsl::span<const Sprite>(sprites).subspan(0, nParticlesAlive);
 }
 
 int Particles::getMask() const
@@ -25,5 +74,67 @@ int Particles::getMask() const
 int Particles::getLayer() const
 {
 	return layer;
+}
+
+void Particles::spawn(size_t n)
+{
+	const size_t start = nParticlesAlive;
+	nParticlesAlive += n;
+	const size_t size = std::max(size_t(8), nextPowerOf2(nParticlesAlive));
+	if (particles.size() < size) {
+		particles.resize(size);
+		sprites.resize(size);
+	}
+	
+	for (size_t i = start; i < nParticlesAlive; ++i) {
+		initializeParticle(i);
+	}
+}
+
+void Particles::initializeParticle(size_t index)
+{	
+	auto& particle = particles[index];
+	particle.alive = true;
+	particle.ttl = 1;
+	particle.pos = getSpawnPosition();
+	particle.angle = Angle1f::fromDegrees(340);
+	particle.vel = Vector2f(rng->getFloat(400, 500), particle.angle);
+
+	auto& sprite = sprites[index];
+	sprite = baseSprite;
+}
+
+void Particles::updateParticles(float time)
+{
+	for (size_t i = 0; i < nParticlesAlive; ++i) {
+		auto& particle = particles[i];
+
+		particle.ttl -= time;
+		if (particle.ttl <= 0) {
+			particle.alive = false;
+		} else {
+			particle.pos += particle.vel * time;
+			particle.angle = particle.vel.angle();
+			
+			sprites[i]
+				.setPosition(particle.pos)
+				.setRotation(particle.angle);
+		}
+	}
+}
+
+Vector2f Particles::getSpawnPosition() const
+{
+	return position + Vector2f(rng->getFloat(-500, 500), rng->getFloat(-200, 200));
+}
+
+ConfigNode ConfigNodeSerializer<Particles>::serialize(const Particles& particles, ConfigNodeSerializationContext& context)
+{
+	return particles.toConfigNode();
+}
+
+Particles ConfigNodeSerializer<Particles>::deserialize(ConfigNodeSerializationContext& context, const ConfigNode& node)
+{
+	return Particles(node, *context.resources);
 }
 
