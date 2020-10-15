247a48be91e3eb90752aedd7b0fb7cf7447d365e
Rodrigo Braz Monteiro <archmagezeratul@gmail.com>
2020-10-15 12:40:57 +0100

    Bump editor version

--- src/engine/core/include/halley/core/entry/entry_point.h
+++ src/engine/core/include/halley/core/entry/entry_point.h
@@ -15,7 +15,7 @@ namespace Halley
 	class Game;
 	class Core;
 
-	constexpr static uint32_t HALLEY_DLL_API_VERSION = 17;
+	constexpr static uint32_t HALLEY_DLL_API_VERSION = 18;
 	
 	class IHalleyEntryPoint
 	{
