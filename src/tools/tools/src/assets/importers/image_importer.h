1d61419684de3bbb98ed7340a5793723f505b33e
Reece Banbury <reece@chucklefish.org>
2020-11-12 13:48:55 +0000

    Added option to only read the top line of a palette when converting

--- src/tools/tools/src/assets/importers/image_importer.h
+++ src/tools/tools/src/assets/importers/image_importer.h
@@ -12,8 +12,8 @@ namespace Halley
 		void import(const ImportingAsset& asset, IAssetCollector& collector) override;
 
 	private:
-		static std::unique_ptr<Image> convertToIndexed(const String& fileName, const Image& image, const Image& palette, const ConfigNode& assetOptions);
-		static std::unordered_map<uint32_t, uint32_t> makePaletteConversion(const Image& palette);
+		static std::unique_ptr<Image> convertToIndexed(const String& fileName, const Image& image, const Image& palette, const ConfigNode& assetOptions, bool readTopLineOnly);
+		static std::unordered_map<uint32_t, uint32_t> makePaletteConversion(const Image& palette, bool readTopLineOnly);
 		static std::pair<String, Vector2i> lookupSpritePosition(const String& fileName, Vector2i pos, const ConfigNode& options);
 	};
 }
