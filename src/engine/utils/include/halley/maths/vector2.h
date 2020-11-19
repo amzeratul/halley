3a25afb8cb6f97f7c0db76b224d831c39edf8a35
Rodrigo Braz Monteiro <archmagezeratul@gmail.com>
2020-11-19 18:16:53 +0000

    Remove move constructor from Vector as it makes clang-lint encourage unnecessary std::move

--- src/engine/utils/include/halley/maths/vector2.h
+++ src/engine/utils/include/halley/maths/vector2.h
@@ -51,7 +51,6 @@ namespace Halley {
 		constexpr Vector2D () noexcept : x(0), y(0) {}
 		constexpr Vector2D (T x, T y) noexcept : x(x), y(y) {}
 		constexpr Vector2D (const Vector2D& vec) noexcept : x(static_cast<T>(vec.x)), y(static_cast<T>(vec.y)) {}
-		constexpr Vector2D (Vector2D&& vec) noexcept : x(static_cast<T>(vec.x)), y(static_cast<T>(vec.y)) {}
 		template <typename V> constexpr explicit Vector2D (const Vector2D<V>& vec) noexcept : x(static_cast<T>(vec.x)), y(static_cast<T>(vec.y)) {}
 
 		constexpr Vector2D (T length, U angle) noexcept
