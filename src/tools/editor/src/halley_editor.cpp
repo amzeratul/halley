300cdcb6b9af71a91daac9d82b4852b3603891e8
Rodrigo Braz Monteiro <archmagezeratul@gmail.com>
2021-03-10 23:49:20 +0000

    Bump version

--- src/tools/editor/src/halley_editor.cpp
+++ src/tools/editor/src/halley_editor.cpp
@@ -111,7 +111,7 @@ std::unique_ptr<Stage> HalleyEditor::startGame()
 {
 	auto& api = getAPI();
 	preferences = std::make_unique<Preferences>();
-	preferences->setEditorVersion("2021-03-06");
+	preferences->setEditorVersion("2021-03-10");
 	preferences->loadFromFile(*api.system);
 
 	projectLoader = std::make_unique<ProjectLoader>(api.core->getStatics(), rootPath, preferences->getDisabledPlatforms());
