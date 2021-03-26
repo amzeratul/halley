4719f999438aa0ca92263dd5acec9b59d2c52f6c
Rodrigo Braz Monteiro <archmagezeratul@gmail.com>
2021-03-26 15:14:54 +0000

    Bump version

--- src/engine/core/include/halley/core/entry/entry_point.h
+++ src/engine/core/include/halley/core/entry/entry_point.h
@@ -16,7 +16,7 @@ namespace Halley
 	class Core;
 	class HalleyStatics;
 
-	constexpr static uint32_t HALLEY_DLL_API_VERSION = 39;
+	constexpr static uint32_t HALLEY_DLL_API_VERSION = 40;
 	
 	class IHalleyEntryPoint
 	{
--- src/tools/editor/src/halley_editor.cpp
+++ src/tools/editor/src/halley_editor.cpp
@@ -111,7 +111,7 @@ std::unique_ptr<Stage> HalleyEditor::startGame()
 {
 	auto& api = getAPI();
 	preferences = std::make_unique<Preferences>();
-	preferences->setEditorVersion("2021-03-24");
+	preferences->setEditorVersion("2021-03-26");
 	preferences->loadFromFile(*api.system);
 
 	projectLoader = std::make_unique<ProjectLoader>(api.core->getStatics(), rootPath, preferences->getDisabledPlatforms());
