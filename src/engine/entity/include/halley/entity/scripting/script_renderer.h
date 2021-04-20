d695b291a186c30930c4db6446b48f90b7cbd9e3
Rodrigo Braz Monteiro <archmagezeratul@gmail.com>
2021-04-20 17:49:44 +0100

    WIP script rendering

--- src/engine/entity/include/halley/entity/scripting/script_renderer.h
+++ src/engine/entity/include/halley/entity/scripting/script_renderer.h
@@ -1,15 +1,24 @@
 #pragma once
+#include "halley/core/graphics/sprite/sprite.h"
+#include "halley/maths/vector2.h"
 
 namespace Halley {
-	class Painter;
+	class ScriptGraphNode;
 	class ScriptGraph;
 
 	class ScriptRenderer {
 	public:
+		explicit ScriptRenderer(Resources& resources);
+		
 		void setGraph(const ScriptGraph& graph);
-		void draw(Painter& painter);
+		void draw(Painter& painter, Vector2f basePos, float curZoom);
 
 	private:
+		Resources& resources;
 		const ScriptGraph* graph = nullptr;
+
+		Sprite nodeBg;
+
+		void drawNode(Painter& painter, Vector2f basePos, const ScriptGraphNode& node, float curZoom);
 	};
 }
--- src/engine/entity/src/scripting/script_renderer.cpp
+++ src/engine/entity/src/scripting/script_renderer.cpp
@@ -1,12 +1,34 @@
 #include "scripting/script_renderer.h"
+
+#include "scripting/script_graph.h"
 using namespace Halley;
+
+ScriptRenderer::ScriptRenderer(Resources& resources)
+	: resources(resources)
+{
+	nodeBg = Sprite().setImage(resources, "halley_ui/ui_float_solid_window.png");
+}
 
 void ScriptRenderer::setGraph(const ScriptGraph& graph)
 {
 	this->graph = &graph;
 }
 
-void ScriptRenderer::draw(Painter& painter)
+void ScriptRenderer::draw(Painter& painter, Vector2f basePos, float curZoom)
 {
-	// TODO
+	if (graph) {
+		for (const auto& node: graph->getNodes()) {
+			drawNode(painter, basePos, node, curZoom);
+		}
+	}
+}
+
+void ScriptRenderer::drawNode(Painter& painter, Vector2f basePos, const ScriptGraphNode& node, float curZoom)
+{
+	const auto pos = basePos + node.getPosition();
+	nodeBg.clone()
+		.setPivot(Vector2f(0.5f, 0.5f))
+		.setPosition(pos)
+		.setScale(1.0f / curZoom)
+		.draw(painter);
 }
