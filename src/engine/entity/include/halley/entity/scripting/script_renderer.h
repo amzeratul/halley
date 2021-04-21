a46a1c319c380dcd2f84db14f20af4923f1e14ea
Rodrigo Braz Monteiro <archmagezeratul@gmail.com>
2021-04-21 14:49:48 +0100

    Render targets and pins

--- src/engine/entity/include/halley/entity/scripting/script_renderer.h
+++ src/engine/entity/include/halley/entity/scripting/script_renderer.h
@@ -3,18 +3,26 @@
 #include "halley/maths/vector2.h"
 
 namespace Halley {
+	class World;
 	class ScriptGraphNode;
 	class ScriptGraph;
 
 	class ScriptRenderer {
 	public:
-		explicit ScriptRenderer(Resources& resources);
+		explicit ScriptRenderer(Resources& resources, World& world);
 		
 		void setGraph(const ScriptGraph& graph);
 		void draw(Painter& painter, Vector2f basePos, float curZoom);
 
 	private:
+		enum class NodeElementType {
+			Input,
+			Output,
+			Target
+		};
+		
 		Resources& resources;
+		World& world;
 		const ScriptGraph* graph = nullptr;
 
 		Sprite nodeBg;
@@ -23,5 +31,6 @@ namespace Halley {
 		void drawNode(Painter& painter, Vector2f basePos, const ScriptGraphNode& node, float curZoom);
 
 		Vector2f getNodeSize(float curZoom) const;
+		Vector2f getNodeElementPosition(NodeElementType type, Vector2f basePos, const ScriptGraphNode& node, size_t elemIdx, float curZoom);
 	};
 }
--- src/engine/entity/src/scripting/script_renderer.cpp
+++ src/engine/entity/src/scripting/script_renderer.cpp
@@ -1,13 +1,18 @@
 #include "scripting/script_renderer.h"
-
-
+#include "world.h"
 #include "halley/core/graphics/painter.h"
 #include "halley/maths/bezier.h"
 #include "scripting/script_graph.h"
 using namespace Halley;
 
-ScriptRenderer::ScriptRenderer(Resources& resources)
+#ifndef DONT_INCLUDE_HALLEY_HPP
+#define DONT_INCLUDE_HALLEY_HPP
+#endif
+#include "components/transform_2d_component.h"
+
+ScriptRenderer::ScriptRenderer(Resources& resources, World& world)
 	: resources(resources)
+	, world(world)
 {
 	nodeBg = Sprite().setImage(resources, "halley_ui/ui_float_solid_window.png");
 }
@@ -35,17 +40,31 @@ void ScriptRenderer::draw(Painter& painter, Vector2f basePos, float curZoom)
 void ScriptRenderer::drawNodeOutputs(Painter& painter, Vector2f basePos, const ScriptGraphNode& node, const ScriptGraph& graph, float curZoom)
 {
 	const Vector2f pos = basePos + node.getPosition();
-	const Vector2f nodeSize = getNodeSize(curZoom);
-	const Vector2f outputPinPos = pos + Vector2f(nodeSize.x * 0.5f, 0.0f) / curZoom;
 
 	for (const auto& output: node.getOutputs()) {
+		const Vector2f srcPos = getNodeElementPosition(NodeElementType::Output, basePos, node, 0, curZoom);
+		
 		const auto& dstNode = graph.getNodes().at(output.nodeId);
-		const Vector2f inputPinPos = basePos + dstNode.getPosition() - Vector2f(nodeSize.x * 0.5f, 0.0f) / curZoom;
+		const Vector2f dstPos = getNodeElementPosition(NodeElementType::Input, basePos, dstNode, 0, curZoom);
 
-		const float dist = std::max(std::abs(inputPinPos.x - outputPinPos.x), 50.0f) / 2;
-		const auto bezier = BezierCubic(outputPinPos, outputPinPos + Vector2f(dist, 0), inputPinPos - Vector2f(dist, 0), inputPinPos);
+		const float dist = std::max(std::abs(dstPos.x - srcPos.x), 50.0f) / 2;
+		const auto bezier = BezierCubic(srcPos, srcPos + Vector2f(dist, 0), dstPos - Vector2f(dist, 0), dstPos);
 		
 		painter.drawLine(bezier, 2.0f / curZoom, Colour4f(1, 1, 1));
+	}
+
+	for (const auto& target: node.getTargets()) {
+		auto entity = world.getEntity(target);
+		auto* transform = entity.tryGetComponent<Transform2DComponent>();
+		if (transform) {
+			const Vector2f srcPos = getNodeElementPosition(NodeElementType::Target, basePos, node, 0, curZoom);
+			const auto dstPos = transform->getGlobalPosition();
+
+			const float dist = std::max(std::abs(dstPos.x - srcPos.x), 50.0f) / 2;
+			const auto bezier = BezierCubic(srcPos, srcPos + Vector2f(0, dist), dstPos - Vector2f(0, dist), dstPos);
+			
+			painter.drawLine(bezier, 1.5f / curZoom, Colour4f(0.35f, 1.0f, 0.35f));
+		}
 	}
 }
 
@@ -65,9 +84,38 @@ void ScriptRenderer::drawNode(Painter& painter, Vector2f basePos, const ScriptGr
 		.setSliceScale(1.0f / curZoom)
 		//.setTexRect(nodeBg.getTexRect() / curZoom)
 		.draw(painter);
+
+	const float radius = 4.0f / curZoom;
+	const float width = 2.0f / curZoom;
+	painter.drawCircle(getNodeElementPosition(NodeElementType::Input, basePos, node, 0, curZoom), radius, width, Colour4f(1, 1, 1));
+	painter.drawCircle(getNodeElementPosition(NodeElementType::Output, basePos, node, 0, curZoom), radius, width, Colour4f(1, 1, 1));
+	if (!node.getTargets().empty()) {
+		painter.drawCircle(getNodeElementPosition(NodeElementType::Target, basePos, node, 0, curZoom), radius, width, Colour4f(0.35f, 1, 0.35f));
+	}
 }
 
 Vector2f ScriptRenderer::getNodeSize(float curZoom) const
 {
 	return Vector2f(60, 60);
+}
+
+Vector2f ScriptRenderer::getNodeElementPosition(NodeElementType type, Vector2f basePos, const ScriptGraphNode& node, size_t elemIdx, float curZoom)
+{
+	const Vector2f nodeSize = getNodeSize(curZoom);
+	
+	Vector2f offset;
+	switch (type) {
+	case NodeElementType::Input:
+		offset = Vector2f(-nodeSize.x * 0.5f, 0.0f);
+		break;
+	case NodeElementType::Output:
+		offset = Vector2f(nodeSize.x * 0.5f, 0.0f);
+		break;
+	case NodeElementType::Target:
+		offset = Vector2f(0.0f, nodeSize.y * 0.5f);
+		break;
+	}
+
+	const Vector2f pos = basePos + node.getPosition();
+	return pos + offset / curZoom;
 }
