ce83dce32a13a8c52929148dd4a26ccc59b2f0af
Rodrigo Braz Monteiro <archmagezeratul@gmail.com>
2021-09-01 12:14:17 +0100

    Bump Halley version

--- src/engine/core/include/halley/core/entry/entry_point.h
+++ src/engine/core/include/halley/core/entry/entry_point.h
@@ -16,7 +16,7 @@ namespace Halley
 	class Core;
 	class HalleyStatics;
 
-	constexpr static uint32_t HALLEY_DLL_API_VERSION = 65;
+	constexpr static uint32_t HALLEY_DLL_API_VERSION = 66;
 	
 	class IHalleyEntryPoint
 	{
