8e6a3714b841e6a864ea91d37a4a891c4e0cbb74
Rodrigo Braz Monteiro <archmagezeratul@gmail.com>
2021-10-15 13:32:45 +0100

    Some fallback to allow deserialization of components outside of a factory context

--- src/engine/entity/src/entity_id.cpp
+++ src/engine/entity/src/entity_id.cpp
@@ -8,11 +8,17 @@ using namespace Halley;
 
 String EntityId::toUUID(const EntityId& id, const ConfigNodeSerializationContext& context)
 {
+	if (!context.entityContext) {
+		return "";
+	}
 	auto& world = context.entityContext->getWorld();
 	return world.getEntity(id).getInstanceUUID().toString();
 }
 
 EntityId EntityId::fromUUID(const String& uuidStr, const ConfigNodeSerializationContext& context)
 {
+	if (!context.entityContext) {
+		return EntityId();
+	}
 	return context.entityContext->getEntityIdFromUUID(UUID(uuidStr));
 }
