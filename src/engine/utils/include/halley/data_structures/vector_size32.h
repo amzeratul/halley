8bf540ea1f6722cf8ddaed5925d4a5bcbaeceef9
Rodrigo Braz Monteiro <archmagezeratul@gmail.com>
2022-02-06 18:31:45 +0000

    A lot more work towards this vector implementation

--- src/engine/utils/include/halley/data_structures/vector_size32.h
+++ src/engine/utils/include/halley/data_structures/vector_size32.h
@@ -7,7 +7,7 @@
 #include <limits>
 
 namespace Halley {
-	template <typename T, typename Allocator = std::allocator<T>>
+	template <typename T, class Allocator = std::allocator<T>>
 	class VectorSize32 {
 	public:
 		using value_type = T;
@@ -17,17 +17,24 @@ namespace Halley {
 		using const_reference = const T&;
 		using pointer = typename std::allocator_traits<Allocator>::pointer;
 		using const_pointer = typename std::allocator_traits<Allocator>::const_pointer;
+		constexpr float growth_factor = 2.0f;
 
+		class const_iterator;
+		
 		class iterator {
+			friend class const_iterator;
+		
 		public:
 			iterator() : v(nullptr) {}
 			iterator(pointer v) : v(v) {}
 			
 			reference operator*() const { return *v; }
-			iterator& operator++() const { ++v; return *this; }
-			iterator operator++(int) const { auto r = *this; ++v; return r; }
-			iterator& operator--() const { --v; return *this; }
-			iterator operator--(int) const { auto r = *this; --v; return r; }
+			iterator& operator++() { ++v; return *this; }
+			iterator& operator--() { --v; return *this; }
+			iterator operator++(int) const { return iterator(v + 1); }
+			iterator operator--(int) const { return iterator(v - 1); }
+			iterator operator+(size_t o) const { return iterator(v + o); }
+			iterator operator-(size_t o) const { return iterator(v - o); }
 
 		private:
 			pointer v;
@@ -37,13 +44,16 @@ namespace Halley {
 		public:
 			const_iterator() : v(nullptr) {}
 			const_iterator(const_pointer v) : v(v) {}
+			const_iterator(iterator v) : v(v.v) {}
 			
 			const_reference operator*() const { return *v; }
-			const_iterator& operator++() const { ++v; return *this; }
-			const_iterator operator++(int) const { auto r = *this; ++v; return r; }
-			const_iterator& operator--() const { --v; return *this; }
-			const_iterator operator--(int) const { auto r = *this; --v; return r; }
-
+			const_iterator& operator++() { ++v; return *this; }
+			const_iterator& operator--() { --v; return *this; }
+			const_iterator operator++(int) const { return const_iterator(v + 1); }
+			const_iterator operator--(int) const { return const_iterator(v - 1); }
+			const_iterator operator+(size_t o) const { return const_iterator(v + o); }
+			const_iterator operator-(size_t o) const { return const_iterator(v - o); }
+		
 		private:
 			const_pointer v;			
 		};
@@ -60,7 +70,11 @@ namespace Halley {
 		
 		VectorSize32(const VectorSize32& other)
 		{
-			// TODO
+			change_capacity(other.m_capacity);
+			m_size = other.m_size;
+			for (uint32_t i = 0; i < m_size; ++i) {
+				new (pointer_at(i)) T(other[i]);
+			}
 		}
 		
 		VectorSize32(VectorSize32&& other) noexcept
@@ -76,12 +90,22 @@ namespace Halley {
 		~VectorSize32()
 		{
 			clear();
-			delete[] m_data;
+			change_capacity(0);
 		}
 
 		VectorSize32& operator=(const VectorSize32& other)
 		{
-			// TODO
+			if (this == &other) {
+				return *this;
+			}
+			
+			change_capacity(other.m_capacity);
+			resize(other.m_size);
+			for (uint32_t i = 0; i < m_size; ++i) {
+				(*this)[i] = other[i];
+			}
+			
+			return *this;
 		}
 
 		VectorSize32& operator=(VectorSize32&& other) noexcept
@@ -95,14 +119,14 @@ namespace Halley {
 			return *this;
 		}
 
-		pointer data()
+		[[nodiscard]] pointer data()
 		{
-			return m_data;
+			return reinterpret_cast<pointer>(m_data);
 		}
 
-		const_pointer data() const
+		[[nodiscard]] const_pointer data() const
 		{
-			return m_data;
+			return reinterpret_cast<const_pointer>(m_data);
 		}
 
 		[[nodiscard]] size_t size() const
@@ -127,12 +151,12 @@ namespace Halley {
 
 		[[nodiscard]] reference operator[](size_t index)
 		{
-			return m_data[index];
+			return data()[index];
 		}
 
 		[[nodiscard]] const_reference operator[](size_t index) const
 		{
-			return m_data[index];			
+			return data()[index];			
 		}
 
 		[[nodiscard]] reference at(size_t index)
@@ -140,7 +164,7 @@ namespace Halley {
 			if (index >= m_size) {
 				[[unlikely]] throw std::out_of_range("Index out of vector range");
 			}
-			return m_data[index];
+			return data()[index];
 		}
 
 		[[nodiscard]] const_reference at(size_t index) const
@@ -148,7 +172,7 @@ namespace Halley {
 			if (index >= m_size) {
 				[[unlikely]] throw std::out_of_range("Index out of vector range");
 			}
-			return m_data[index];			
+			return data()[index];			
 		}
 
 		[[nodiscard]] reference front()
@@ -175,66 +199,109 @@ namespace Halley {
 			return (*this)[m_size - 1];			
 		}
 
-		void resize(size_t size)
+		void resize(size_t size, T defaultValue = T())
 		{
-			// TODO
+			const auto newSize = static_cast<uint32_t>(size);
+			if (newSize > m_size) {
+				if (newSize > m_capacity) {
+					change_capacity(newSize);
+				}
+				for (size_type i = m_size; i < newSize; ++i) {
+					new (pointer_at(i)) T(defaultValue);
+				}
+				m_size = newSize;
+			} else if (newSize < m_size) {
+				for (size_type i = newSize; i < m_size; ++i) {
+					elem(i).~T();
+				}
+				m_size = newSize;
+			}
 		}
 
 		void reserve(size_t size)
 		{
-			// TODO
+			change_capacity(std::max(static_cast<size_type>(size), m_capacity));
 		}
 
 		void shrink_to_fit()
 		{
-			// TODO
+			change_capacity(m_size);
 		}
 
 		void clear()
 		{
-			// TODO
+			resize(0);
 		}
 
 		iterator insert(const_iterator pos, const T& value)
 		{
-			// TODO
+			push_back(value);
+			std::rotate(pos, end() - 1, end());
+			return pos;
 		}
 
 		iterator insert(const_iterator pos, T&& value)
 		{
-			// TODO
-		}
-
-		iterator erase(const_iterator pos)
-		{
-			// TODO
+			push_back(std::move(value));
+			std::rotate(pos, end() - 1, end());
+			return pos;
 		}
 
 		template <class... Args>
 		iterator emplace(const_iterator pos, Args&&... args)
 		{
-			// TODO
+			emplace_back(std::forward(args));
+			std::rotate(pos, end() - 1, end());
+			return pos;
+		}
+
+		iterator erase(const_iterator first, const_iterator last)
+		{
+			std::rotate(first, last, end());
+			do_resize(m_size - (last - first), m_capacity);
+			return first; // ?
+		}
+
+		iterator erase(const_iterator pos)
+		{
+			return erase(pos, pos + 1);
 		}
 
 		template <class... Args>
 		reference emplace_back(Args&&... args)
 		{
-			// TODO
+			const auto idx = m_size;
+			construct_with_ensure_capacity(m_size + 1, [&] (std::byte* data)
+			{
+				new(pointer_at(idx, data)) T(args);			
+			});
+			++m_size;
+			return elem(idx);
 		}
 
 		void push_back(const T& value)
 		{
-			// TODO
+			construct_with_ensure_capacity(m_size + 1, [&](std::byte* data)
+			{
+				new(pointer_at(m_size, data)) T(value);
+			});
+			++m_size;
 		}
 
 		void push_back(T&& value)
 		{
-			// TODO
+			construct_with_ensure_capacity(m_size + 1, [&](std::byte* data)
+			{
+				new(pointer_at(m_size, data)) T(std::move(value));
+			});
+			++m_size;
 		}
 
 		void pop_back()
 		{
-			// TODO
+			assert(m_size > 0);
+			elem(m_size - 1).~T();
+			--m_size;
 		}
 
 		void swap(VectorSize32& other) noexcept
@@ -244,49 +311,125 @@ namespace Halley {
 			std::swap(m_capacity, other.m_capacity);
 		}
 
-		iterator begin()
+		[[nodiscard]] iterator begin()
 		{
-			return iterator(&data()[0]);
+			return iterator(&elem(0));
 		}
 
-		iterator end()
+		[[nodiscard]] iterator end()
 		{
-			return iterator(&data()[m_size]);
+			return iterator(&elem(m_size));
 		}
 
-		const_iterator begin() const
+		[[nodiscard]] const_iterator begin() const
 		{
-			return const_iterator(&data()[0]);
+			return const_iterator(&elem(0));
 		}
 
-		const_iterator end() const
+		[[nodiscard]] const_iterator end() const
 		{
-			return const_iterator(&data()[m_size]);
+			return const_iterator(&elem(m_size));
 		}
 
-		reverse_iterator rbegin()
+		[[nodiscard]] reverse_iterator rbegin()
 		{
 			return reverse_iterator(end())++;
 		}
 
-		reverse_iterator rend()
+		[[nodiscard]] reverse_iterator rend()
 		{
 			return reverse_iterator(begin())++;
 		}
 
-		const_reverse_iterator rbegin() const
+		[[nodiscard]] const_reverse_iterator rbegin() const
 		{
 			return const_reverse_iterator(end())++;
 		}
 
-		const_reverse_iterator rend() const
+		[[nodiscard]] const_reverse_iterator rend() const
 		{
 			return const_reverse_iterator(begin())++;
 		} 
 
 	private:
-		pointer m_data;
+		std::byte* m_data;
 		size_type m_size;
 		size_type m_capacity;
+
+		template <typename F>
+		void change_capacity(size_type newCapacity, const F& construct)
+		{
+			assert(newCapacity >= m_size);
+			if (newCapacity != m_capacity) {
+				std::byte* newData = newCapacity > 0 ? new std::byte[newCapacity * sizeof(T)] : nullptr;
+
+				construct(newData);
+				
+				for (size_type i = 0; i < m_size; ++i) {
+					new (pointer_at(i, newData)) T(std::move(elem(i)));
+					elem(i).~T();
+				}
+				delete[] m_data;
+				m_data = newData;
+			}
+		}
+
+		template <typename F>
+		void construct_with_ensure_capacity(size_type minCapacity, const F& construct)
+		{
+			if (m_capacity >= minCapacity) {
+				construct(m_data);
+			} else {
+				change_capacity(std::max(minCapacity, static_cast<size_type>(m_capacity * growth_factor)), construct);
+			}
+		}
+
+		[[nodiscard]] std::byte* pointer_at(size_type pos, std::byte* bytes)
+		{
+			return bytes + pos * sizeof(T);
+		}
+
+		[[nodiscard]] std::byte* pointer_at(size_type pos)
+		{
+			return m_data + pos * sizeof(T);
+		}
+
+		[[nodiscard]] reference elem(size_type pos)
+		{
+			return (*data)[pos];
+		}
+
+		[[nodiscard]] const_reference elem(size_type pos) const
+		{
+			return (*data)[pos];
+		}
 	};
+
+	template <typename T, class Allocator>
+	bool operator==(const VectorSize32<T, Allocator>& a, const VectorSize32<T, Allocator>& b)
+	{
+		if (a.size() != b.size()) {
+			return false;
+		}
+		for (size_t i = 0; i < a.size(); ++i) {
+			if (!(a[i] == b[i])) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	template <typename T, class Allocator>
+	bool operator!=(const VectorSize32<T, Allocator>& a, const VectorSize32<T, Allocator>& b)
+	{
+		if (a.size() != b.size()) {
+			return true;
+		}
+		for (size_t i = 0; i < a.size(); ++i) {
+			if (a[i] != b[i]) {
+				return true;
+			}
+		}
+		return false;
+	}
 }
