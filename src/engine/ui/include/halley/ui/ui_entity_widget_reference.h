1ab5a7e6ab2a3512529d4937fe05861355e916de
Rodrigo Braz Monteiro <archmagezeratul@gmail.com>
2022-07-04 12:30:57 +0100

    Add alive flag

--- src/engine/ui/include/halley/ui/ui_entity_widget_reference.h
+++ src/engine/ui/include/halley/ui/ui_entity_widget_reference.h
@@ -9,6 +9,8 @@ namespace Halley {
         Vector2f alignment;
         std::shared_ptr<UIWidget> widget;
 
+        bool alive = true;
+
         UIEntityWidgetReference() = default;
         UIEntityWidgetReference(String id, Vector2f offset, Vector2f alignment, std::shared_ptr<UIWidget> widget);
     };
