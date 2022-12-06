530a612c36ace92557175089ca8abfeee3e857a0
Rodrigo Braz Monteiro <archmagezeratul@gmail.com>
2022-12-06 22:55:01 +0000

    Improved shader

--- shared_assets/shader/frame_debug_view.pixel.hlsl
+++ shared_assets/shader/frame_debug_view.pixel.hlsl
@@ -3,11 +3,9 @@
 float4 main(VOut input) : SV_TARGET {
 	float2 vertPos = input.vertPos;
 	float2 d = float2(abs(ddx(vertPos.x)), abs(ddy(vertPos.y)));
-	float x0 = vertPos.x - d.x;
-	float x1 = vertPos.x + d.x;
-	float y0 = vertPos.y - d.y;
-	float y1 = vertPos.y + d.y;
-	if (x0 < 0 || x1 >= 1 || y0 < 0 || y1 >= 1) {
+
+	float dist = max(max(d.x - vertPos.x, d.x + vertPos.x - 1), max(d.y - vertPos.y, d.y + vertPos.y - 1));
+	if (dist >= 0) {
 		return float4(1, 0, 0, 1);
 	}
 
