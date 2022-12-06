99cdbeb506c6ca86cd5f68b1ab7dc8911b0903f3
Rodrigo Braz Monteiro <archmagezeratul@gmail.com>
2022-12-06 22:40:08 +0000

    Outlines on debug view shader

--- shared_assets/shader/frame_debug_view.pixel.hlsl
+++ shared_assets/shader/frame_debug_view.pixel.hlsl
@@ -1,5 +1,15 @@
 #include "halley/sprite_attribute.hlsl"
 
 float4 main(VOut input) : SV_TARGET {
+	float2 vertPos = input.vertPos;
+	float2 d = float2(abs(ddx(vertPos.x)), abs(ddy(vertPos.y)));
+	float x0 = vertPos.x - d.x;
+	float x1 = vertPos.x + d.x;
+	float y0 = vertPos.y - d.y;
+	float y1 = vertPos.y + d.y;
+	if (x0 < 0 || x1 >= 1 || y0 < 0 || y1 >= 1) {
+		return float4(1, 0, 0, 1);
+	}
+
 	return float4(1, 0, 0, 0.25);
 }
