2aa7176a5762b0fe5477e9c9d979b577ccfe71c5
Daniel Ludwig <codi@code-disaster.com>
2023-01-25 14:56:34 +0100

    Rename shader uniform to match material definition

--- shared_assets/shader/text_offset.vertex.hlsl
+++ shared_assets/shader/text_offset.vertex.hlsl
@@ -4,14 +4,14 @@
 cbuffer MaterialBlock : register(b1) {
     float u_smoothness;
     float u_outline;
-    float u_shadowDist;
+    float u_shadowDistance;
     float u_shadowSmoothness;
     float4 u_outlineColour;
     float4 u_shadowColour;
 };
 
 VOut main(VIn input) {
-    input.position += u_shadowDist.xx;
+    input.position += u_shadowDistance.xx;
     VOut result;
     basicVertex(input, result);
     return result;
--- shared_assets/shader/text_outline.pixel.hlsl
+++ shared_assets/shader/text_outline.pixel.hlsl
@@ -8,7 +8,7 @@ SamplerState sampler0 : register(s0) {
 cbuffer MaterialBlock : register(b1) {
     float u_smoothness;
     float u_outline;
-    float u_shadowDist;
+    float u_shadowDistance;
     float u_shadowSmoothness;
     float4 u_outlineColour;
     float4 u_shadowColour;
