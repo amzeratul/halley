4ba6a3a62e01e8c33d1c42f313f90666e2fc6c56
Rodrigo Braz Monteiro <archmagezeratul@gmail.com>
2023-02-01 16:36:43 +0000

    Script active states are now stored as a HashMap for better serialization

--- shared_gen/cpp/components/scriptable_component.h
+++ shared_gen/cpp/components/scriptable_component.h
@@ -1,4 +1,4 @@
-// Halley codegen version 116
+// Halley codegen version 118
 #pragma once
 
 #ifndef DONT_INCLUDE_HALLEY_HPP
@@ -10,7 +10,7 @@ public:
 	static constexpr int componentIndex{ 8 };
 	static const constexpr char* componentName{ "Scriptable" };
 
-	Halley::Vector<std::shared_ptr<Halley::ScriptState>> activeStates{};
+	Halley::HashMap<Halley::String, std::shared_ptr<Halley::ScriptState>> activeStates{};
 	Halley::Vector<Halley::String> tags{};
 	Halley::Vector<Halley::ResourceReference<Halley::ScriptGraph>> scripts{};
 	Halley::ScriptVariables variables{};
@@ -27,7 +27,7 @@ public:
 	Halley::ConfigNode serialize(const Halley::EntitySerializationContext& context) const {
 		using namespace Halley::EntitySerialization;
 		Halley::ConfigNode node = Halley::ConfigNode::MapType();
-		Halley::EntityConfigNodeSerializer<decltype(activeStates)>::serialize(activeStates, Halley::Vector<std::shared_ptr<Halley::ScriptState>>{}, context, node, componentName, "activeStates", makeMask(Type::Network));
+		Halley::EntityConfigNodeSerializer<decltype(activeStates)>::serialize(activeStates, Halley::HashMap<Halley::String, std::shared_ptr<Halley::ScriptState>>{}, context, node, componentName, "activeStates", makeMask(Type::Network));
 		Halley::EntityConfigNodeSerializer<decltype(tags)>::serialize(tags, Halley::Vector<Halley::String>{}, context, node, componentName, "tags", makeMask(Type::Prefab, Type::SaveData, Type::Network));
 		Halley::EntityConfigNodeSerializer<decltype(scripts)>::serialize(scripts, Halley::Vector<Halley::ResourceReference<Halley::ScriptGraph>>{}, context, node, componentName, "scripts", makeMask(Type::Prefab));
 		Halley::EntityConfigNodeSerializer<decltype(variables)>::serialize(variables, Halley::ScriptVariables{}, context, node, componentName, "variables", makeMask(Type::SaveData, Type::Network));
@@ -36,7 +36,7 @@ public:
 
 	void deserialize(const Halley::EntitySerializationContext& context, const Halley::ConfigNode& node) {
 		using namespace Halley::EntitySerialization;
-		Halley::EntityConfigNodeSerializer<decltype(activeStates)>::deserialize(activeStates, Halley::Vector<std::shared_ptr<Halley::ScriptState>>{}, context, node, componentName, "activeStates", makeMask(Type::Network));
+		Halley::EntityConfigNodeSerializer<decltype(activeStates)>::deserialize(activeStates, Halley::HashMap<Halley::String, std::shared_ptr<Halley::ScriptState>>{}, context, node, componentName, "activeStates", makeMask(Type::Network));
 		Halley::EntityConfigNodeSerializer<decltype(tags)>::deserialize(tags, Halley::Vector<Halley::String>{}, context, node, componentName, "tags", makeMask(Type::Prefab, Type::SaveData, Type::Network));
 		Halley::EntityConfigNodeSerializer<decltype(scripts)>::deserialize(scripts, Halley::Vector<Halley::ResourceReference<Halley::ScriptGraph>>{}, context, node, componentName, "scripts", makeMask(Type::Prefab));
 		Halley::EntityConfigNodeSerializer<decltype(variables)>::deserialize(variables, Halley::ScriptVariables{}, context, node, componentName, "variables", makeMask(Type::SaveData, Type::Network));
