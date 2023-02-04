9f190dbd99eae7b3079a177189bbb3cddc80e4b3
Rodrigo Braz Monteiro <archmagezeratul@gmail.com>
2023-02-04 12:42:11 +0000

    Make Halley/SpriteOpaque be truly opaque

---
+++ shared_assets/shader/sprite_opaque.pixel.hlsl
@@ -0,0 +1,9 @@
+#include "halley/sprite_attribute.hlsl"
+
+Texture2D tex0 : register(t0);
+SamplerState sampler0 : register(s0);
+
+float4 main(VOut input) : SV_TARGET {
+	float4 col = tex0.Sample(sampler0, input.texCoord0.xy);
+	return col.rgb1 * input.colour.rgb1 + input.colourAdd;
+}
