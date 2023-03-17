469d42e4323258e1a9434008a3f383aedb5d7c27
Rodrigo Braz Monteiro <archmagezeratul@gmail.com>
2023-03-17 17:50:34 +0000

    Fix namespace declaration for zip_file.h

--- src/engine/core/include/halley/file_formats/zip_file.h
+++ src/engine/core/include/halley/file_formats/zip_file.h
@@ -1,41 +1,41 @@
 #pragma once
 #include "halley/file/path.h"
 
-using namespace Halley;
+namespace Halley {
+	class FreeDeleter {
+	public:
+		void operator()(void* ptr) const
+		{
+			free(ptr);
+		}
+	};
 
-class FreeDeleter {
-public:
-    void operator()(void* ptr) const
-    {
-        free(ptr);
-    }
-};
+	class ZipFile {
+	public:
+		ZipFile();
+		ZipFile(Path path, bool inMemory);
+		~ZipFile();
 
-class ZipFile {
-public:
-    ZipFile();
-    ZipFile(Path path, bool inMemory);
-    ~ZipFile();
+		bool open(Path path, bool inMemory);
+		bool open(Bytes bytes);
+		void close();
 
-    bool open(Path path, bool inMemory);
-    bool open(Bytes bytes);
-    void close();
+		size_t getNumFiles() const;
+		String getFileName(size_t idx) const;
+		Vector<String> getFileNames() const;
+		Bytes extractFile(size_t idx) const;
 
-    size_t getNumFiles() const;
-    String getFileName(size_t idx) const;
-    Vector<String> getFileNames() const;
-    Bytes extractFile(size_t idx) const;
+		void printDiagnostics() const;
 
-    void printDiagnostics() const;
+		static bool isZipFile(const Path& path);
+		static Bytes readFile(const Path& path);
 
-    static bool isZipFile(const Path& path);
-    static Bytes readFile(const Path& path);
+	private:
+		Path path;
+		bool isOpen = false;
 
-private:
-    Path path;
-	bool isOpen = false;
-
-	void* fileHandle = nullptr;
-    std::unique_ptr<void, FreeDeleter> archive = nullptr;
-    Bytes compressedData;
-};
+		void* fileHandle = nullptr;
+		std::unique_ptr<void, FreeDeleter> archive = nullptr;
+		Bytes compressedData;
+	};
+}
