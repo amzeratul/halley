7120ea2ca362c1e09a33ac7f510bf129505ae2b9
Daniel Ludwig <codi@code-disaster.com>
2023-03-30 11:01:08 +0200

    Same error, different compiler (GCC)

--- src/engine/core/include/halley/bytes/compression.h
+++ src/engine/core/include/halley/bytes/compression.h
@@ -24,7 +24,7 @@ namespace Halley {
 		struct LZ4Options {
 			LZ4Mode mode = LZ4Mode::Normal;
 			int level = 9;
-#if defined(__clang__)
+#if defined(__clang__) || defined(__GNUC__)
             // work around "error: default member initializer for 'mode' needed within definition
             // of enclosing class 'Compression' outside of member functions"
             LZ4Options() noexcept {}
