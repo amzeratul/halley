4669c9ff72b470d0ede7967043374c18e1ce5e6e
Rodrigo Braz Monteiro <archmagezeratul@gmail.com>
2023-05-03 17:48:40 +0100

    Network lock is now a map

--- shared_gen/cpp/components/network_component.h
+++ shared_gen/cpp/components/network_component.h
@@ -12,7 +12,7 @@ public:
 
 	std::optional<uint8_t> ownerId{};
 	Halley::DataInterpolatorSet dataInterpolatorSet{};
-	std::optional<uint8_t> lockOwnerId{};
+	Halley::HashMap<Halley::EntityId, uint8_t> locks{};
 	bool sendUpdates{ false };
 
 	NetworkComponent() {
@@ -21,14 +21,14 @@ public:
 	Halley::ConfigNode serialize(const Halley::EntitySerializationContext& context) const {
 		using namespace Halley::EntitySerialization;
 		Halley::ConfigNode node = Halley::ConfigNode::MapType();
-		Halley::EntityConfigNodeSerializer<decltype(lockOwnerId)>::serialize(lockOwnerId, std::optional<uint8_t>{}, context, node, componentName, "lockOwnerId", makeMask(Type::Network));
+		Halley::EntityConfigNodeSerializer<decltype(locks)>::serialize(locks, Halley::HashMap<Halley::EntityId, uint8_t>{}, context, node, componentName, "locks", makeMask(Type::Network));
 		Halley::EntityConfigNodeSerializer<decltype(sendUpdates)>::serialize(sendUpdates, bool{ false }, context, node, componentName, "sendUpdates", makeMask(Type::SaveData, Type::Network));
 		return node;
 	}
 
 	void deserialize(const Halley::EntitySerializationContext& context, const Halley::ConfigNode& node) {
 		using namespace Halley::EntitySerialization;
-		Halley::EntityConfigNodeSerializer<decltype(lockOwnerId)>::deserialize(lockOwnerId, std::optional<uint8_t>{}, context, node, componentName, "lockOwnerId", makeMask(Type::Network));
+		Halley::EntityConfigNodeSerializer<decltype(locks)>::deserialize(locks, Halley::HashMap<Halley::EntityId, uint8_t>{}, context, node, componentName, "locks", makeMask(Type::Network));
 		Halley::EntityConfigNodeSerializer<decltype(sendUpdates)>::deserialize(sendUpdates, bool{ false }, context, node, componentName, "sendUpdates", makeMask(Type::SaveData, Type::Network));
 	}
 
