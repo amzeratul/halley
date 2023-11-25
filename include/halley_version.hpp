2849518f3645e3d16ba928c3f2b6e07d2aa8268b
Rodrigo Braz Monteiro <archmagezeratul@gmail.com>
2023-11-25 16:47:40 +0000

    Bump version

--- include/halley_version.hpp
+++ include/halley_version.hpp
@@ -1,4 +1,4 @@
 // This file is read by halley-launcher, do not modify its structure
 #define HALLEY_VERSION_MAJOR 3
 #define HALLEY_VERSION_MINOR 4
-#define HALLEY_VERSION_REVISION 118
+#define HALLEY_VERSION_REVISION 119
