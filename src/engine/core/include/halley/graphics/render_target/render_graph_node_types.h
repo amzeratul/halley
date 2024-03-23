7730961739b28aab8c9400020327b04c7fce0ed0
Rodrigo Braz Monteiro <archmagezeratul@gmail.com>
2024-03-23 22:11:21 +0000

    Some colours

--- src/engine/core/include/halley/graphics/render_target/render_graph_node_types.h
+++ src/engine/core/include/halley/graphics/render_target/render_graph_node_types.h
@@ -4,9 +4,20 @@
 #include "halley/graph/base_graph_type.h"
 
 namespace Halley {
+	enum class RenderGraphNodeClassification {
+		DrawPass,
+		Filter,
+		Sink,
+		Texture
+	};
+
 	class RenderGraphNodeType : public IGraphNodeType {
 	public:
 		gsl::span<const PinType> getPinConfiguration(const BaseGraphNode& node) const override;
+
+		virtual RenderGraphNodeClassification getClassification() const = 0;
+
+		Colour4f getColour() const override;
 	};
 
 	namespace RenderGraphNodeTypes {
@@ -16,30 +27,35 @@ namespace Halley {
 		public:
 			String getId() const override { return "paint"; }
 			String getName() const override { return "Paint"; }
+			RenderGraphNodeClassification getClassification() const override { return RenderGraphNodeClassification::DrawPass; }
 		};
 
 		class OverlayNodeType : public RenderGraphNodeType {
 		public:
 			String getId() const override { return "overlay"; }
 			String getName() const override { return "Overlay"; }
+			RenderGraphNodeClassification getClassification() const override { return RenderGraphNodeClassification::Filter; }
 		};
 
 		class RenderToTextureNodeType : public RenderGraphNodeType {
 		public:
 			String getId() const override { return "renderToTexture"; }
 			String getName() const override { return "Render to Texture"; }
+			RenderGraphNodeClassification getClassification() const override { return RenderGraphNodeClassification::Sink; }
 		};
 
 		class OutputNodeType : public RenderGraphNodeType {
 		public:
 			String getId() const override { return "output"; }
 			String getName() const override { return "Output"; }
+			RenderGraphNodeClassification getClassification() const override { return RenderGraphNodeClassification::Sink; }
 		};
 
 		class ImageOutputNodeType : public RenderGraphNodeType {
 		public:
 			String getId() const override { return "imageOutput"; }
 			String getName() const override { return "Image Output"; }
+			RenderGraphNodeClassification getClassification() const override { return RenderGraphNodeClassification::Texture; }
 		};
 	}
 }
--- src/engine/core/src/graphics/render_target/render_graph_node_types.cpp
+++ src/engine/core/src/graphics/render_target/render_graph_node_types.cpp
@@ -7,6 +7,21 @@ gsl::span<const RenderGraphNodeType::PinType> RenderGraphNodeType::getPinConfigu
 	return {};
 }
 
+Colour4f RenderGraphNodeType::getColour() const
+{
+	switch (getClassification()) {
+	case RenderGraphNodeClassification::DrawPass:
+		return Colour4f(0.07f, 0.84f, 0.09f);
+	case RenderGraphNodeClassification::Filter:
+		return Colour4f(0.35f, 0.55f, 0.97f);
+	case RenderGraphNodeClassification::Sink:
+		return Colour4f(0.97f, 0.35f, 0.35f);
+	case RenderGraphNodeClassification::Texture:
+		return Colour4f(0.91f, 0.71f, 0.0f);
+	}
+	return Colour4f(0.2f, 0.2f, 0.2f);
+}
+
 std::shared_ptr<GraphNodeTypeCollection> RenderGraphNodeTypes::makeRenderGraphTypes()
 {
 	auto result = std::make_shared<GraphNodeTypeCollection>();
