e0c66e5811b54861c20a82c9981ffc89584a7ec3
Rodrigo Braz Monteiro <archmagezeratul@gmail.com>
2024-09-26 21:21:18 +0100

    Fix shader

--- shared_assets/shader/sprite_solid_colour.pixel.hlsl
+++ shared_assets/shader/sprite_solid_colour.pixel.hlsl
@@ -7,5 +7,5 @@ float4 main(VOut input) : SV_TARGET {
 	float4 col = image.Sample(sampler0, input.texCoord0.xy);
 
 	float alpha = col.a * input.colour.a;
-	return float4(clamp(input.colour.rgb + input.colourAdd.rgb * alpha, float3(0, 0, 0), float3(alpha, alpha, alpha)), alpha);
+	return float4(input.colour.rgb * alpha, alpha);
 }
