9dcc0994be81100311f9a53db77396dc5f5883c7
Daniel Ludwig <codi@code-disaster.com>
2025-01-13 10:43:23 +0100

    Implement stub serialize function

--- src/engine/core/include/halley/timeline/timeline_player.h
+++ src/engine/core/include/halley/timeline/timeline_player.h
@@ -4,6 +4,8 @@
 
 namespace Halley {
     class TimelinePlayer {
-
+    public:
+        void serialize(Serializer& serializer) const;
+        void deserialize(Deserializer& deserializer);
     };
 }
--- src/engine/core/src/timeline/timeline_player.cpp
+++ src/engine/core/src/timeline/timeline_player.cpp
@@ -1,3 +1,13 @@
 #include "halley/timeline/timeline_player.h"
 
 using namespace Halley;
+
+void TimelinePlayer::serialize(Serializer& serializer) const
+{
+
+}
+
+void TimelinePlayer::deserialize(Deserializer& deserializer)
+{
+
+}
