753095bd3494db38192a50917141ef104746ec17
Daniel Ludwig <codi@code-disaster.com>
2025-01-22 11:54:07 +0100

    Log error instead of exception for duplicate UUID, but old entity is empty

--- src/engine/core/src/entity/world.cpp
+++ src/engine/core/src/entity/world.cpp
@@ -240,7 +240,13 @@ EntityRef World::createEntity(UUID uuid, String name, std::optional<EntityRef> p
 		if (oldEntity->getInstanceUUID() != uuid) {
 			throw Exception("Error creating entity \"" + name + "\" - World::uuidMap is seemingly corrupted", HalleyExceptions::Entity);
 		} else {
-			throw Exception("Error creating entity \"" +name + "\" - UUID " + toString(uuid) + " already exists as " + (oldEntity->name ? *oldEntity->name : String()), HalleyExceptions::Entity);
+			if (oldEntity->parent || !oldEntity->children.empty() || !oldEntity->components.empty()) {
+				throw Exception("Error creating entity \"" +name + "\" - UUID " + toString(uuid) + " already exists as " + (oldEntity->name ? *oldEntity->name : String()), HalleyExceptions::Entity);
+			} else {
+				// NOTE: Currently this can happen on network clients sometimes. An empty entity
+				// seems to be alive still. For now, log an error instead of throwing an exception.
+				Logger::logError("Error creating entity \"" +name + "\" - UUID " + toString(uuid) + " already exists as " + (oldEntity->name ? *oldEntity->name : String()));
+			}
 		}
 	}
 	
