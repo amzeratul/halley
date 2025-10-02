3f14aafffe59ddd58618edcb15776808a123dfa9
Rodrigo Braz Monteiro <archmagezeratul@gmail.com>
2025-10-02 13:26:34 +0100

    Fix map smooth pixel art resize shader

--- shared_assets/shader/smooth_pixel.pixel.hlsl
+++ shared_assets/shader/smooth_pixel.pixel.hlsl
@@ -7,16 +7,16 @@ cbuffer TexParams : register(b1) {
     float2 u_texSize0;
 };
 
-float4 smoothPixel(Texture2D tex, float2 uv, float sharpness)
+float4 smoothPixel(Texture2D tex, float2 uv, float sharpness, float4 texRect)
 {
     float2 size = u_texSize0;
     float2 pxCoord = size * uv;
     float pxSize = fwidth(pxCoord.x);
-    float2 sampleCoord = (floor(pxCoord) + smoothstep(0, 1, frac(pxCoord) / pxSize) - 0.5) / size;
+    float2 sampleCoord = clamp((floor(pxCoord) + smoothstep(0, 1, frac(pxCoord) / pxSize) - 0.5) / size, texRect.xy, texRect.zw);
     return tex.Sample(sampler0, lerp(uv, sampleCoord, smoothstep(1, 2, 1.0 / pxSize)));
 }
 
 float4 main(VOut input) : SV_TARGET {
-    float4 col = smoothPixel(image, input.texCoord0.xy, 1.0);
+    float4 col = smoothPixel(image, input.texCoord0.xy, 1.0, input.texCoord0Bounds);
     return col * input.colour + input.colourAdd * col.a;
 }
