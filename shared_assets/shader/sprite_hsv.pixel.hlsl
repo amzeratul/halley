ef7420007cef3e7af52fcf9c5067777915403557
Rodrigo Braz Monteiro <archmagezeratul@gmail.com>
2025-11-06 15:15:01 +0000

    SpriteHSV material

---
+++ shared_assets/shader/sprite_hsv.pixel.hlsl
@@ -0,0 +1,15 @@
+#include "halley/sprite_attribute.hlsl"
+#include "halley/colour.hlsl"
+
+Texture2D image : register(t0);
+SamplerState sampler0 : register(s0);
+
+float4 main(VOut input) : SV_TARGET {
+	float4 col = image.Sample(sampler0, input.texCoord0.xy);
+
+    float3 hsvAdjust = input.custom0.xyz;
+	col.rgb = hsvToRgb(rgbToHsv(col.rgb) + hsvAdjust);
+
+	float alpha = col.a * input.colour.a;
+	return float4(clamp(col.rgb * input.colour.rgb + input.colourAdd.rgb * alpha, float3(0, 0, 0), float3(alpha, alpha, alpha)), alpha);
+}
