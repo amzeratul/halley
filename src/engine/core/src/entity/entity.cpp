33e1379fa30bbb2e4984482cd063efa2870c8dc1
Daniel Ludwig <codi@code-disaster.com>
2026-04-08 14:58:42 +0200

    Print entity name in dev log

--- src/engine/core/src/entity/entity.cpp
+++ src/engine/core/src/entity/entity.cpp
@@ -372,8 +372,8 @@ DataInterpolatorSet& Entity::setupNetwork(EntityRef& ref, uint8_t peerId)
         if (!networkComponent->ownerId) {
             networkComponent->ownerId = peerId;
         } else if (networkComponent->ownerId != peerId) {
-            Logger::logDev("Ignore attempt to reassign ownership of " + toString(entityId.value & 0xffffffff) +
-            	" from peer #" + toString(int(networkComponent->ownerId.value())) + " to #" + toString(int(peerId)) + ".");
+            Logger::logDev("Ignore attempt to reassign ownership of " + ref.getName() + "(" + toString(entityId.value & 0xffffffff) +
+            	") from peer #" + toString(int(networkComponent->ownerId.value())) + " to #" + toString(int(peerId)) + ".");
         }
 		return networkComponent->dataInterpolatorSet;
 	} else {
