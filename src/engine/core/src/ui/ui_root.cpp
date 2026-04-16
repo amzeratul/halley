7cc4528e81541e86029cb92ee9ae3eb17bb5fd8a
Rodrigo Braz Monteiro <archmagezeratul@gmail.com>
2026-04-16 14:22:23 +0100

    More tracing

--- src/engine/core/src/ui/ui_root.cpp
+++ src/engine/core/src/ui/ui_root.cpp
@@ -289,7 +289,8 @@ void UIRoot::updateWidgets(UIWidgetUpdateType type, Time t, UIInputType activeIn
 
 	for (size_t i = 0; i < widgetsCache.size(); ++i) {
 		auto& w = widgetsCache[i];
-		const auto trace = StackDebugTrace("widgetId", w->getId());
+		const auto trace2 = StackDebugTrace("widgetClass", typeid(w).name());
+		const auto trace1 = StackDebugTrace("widgetId", w->getId());
 		if (w->getParent() && w->getParent()->isGuardedUpdate()) {
 			bool crashed = false;
 			try {
