b6f9127cae702635c674e3bda8abfb2364f0a8bf
Rodrigo Braz Monteiro <archmagezeratul@gmail.com>
2026-06-17 17:21:57 +0100

    Inline XXH3 for small data types

--- src/engine/core/include/halley/utils/hash.h
+++ src/engine/core/include/halley/utils/hash.h
@@ -38,7 +38,23 @@ namespace Halley {
 			template<typename T, std::enable_if_t<std::is_trivially_copyable_v<T>, int> = 0>
 			void feed(const T& data)
 			{
-				feedBytes(gsl::as_bytes(gsl::span<const T>(&data, 1)));
+				if constexpr (sizeof(T) == 1) {
+					feed1Byte(&data);
+				} else if constexpr (sizeof(T) == 2) {
+					feed2Bytes(&data);
+				} else if constexpr (sizeof(T) == 4) {
+					feed4Bytes(&data);
+				} else if constexpr (sizeof(T) == 8) {
+					feed8Bytes(&data);
+				} else if constexpr (sizeof(T) == 16) {
+					feed16Bytes(&data);
+				} else if constexpr (sizeof(T) == 32) {
+					feed32Bytes(&data);
+				} else if constexpr (sizeof(T) == 64) {
+					feed64Bytes(&data);
+				} else {
+					feedBytes(gsl::as_bytes(gsl::span<const T>(&data, 1)));
+				}
 			}
 
 			void feedBytes(gsl::span<const std::byte> bytes);
@@ -47,7 +63,15 @@ namespace Halley {
 			void reset();
 
 		private:
-			void* data;
+			void* state;
+
+			void feed1Byte(const void* bytes);
+			void feed2Bytes(const void* bytes);
+			void feed4Bytes(const void* bytes);
+			void feed8Bytes(const void* bytes);
+			void feed16Bytes(const void* bytes);
+			void feed32Bytes(const void* bytes);
+			void feed64Bytes(const void* bytes);
 		};
     };
 }
--- src/engine/core/src/utils/hash.cpp
+++ src/engine/core/src/utils/hash.cpp
@@ -1,4 +1,6 @@
 #include "halley/utils/hash.h"
+
+#define XXH_INLINE_ALL
 #include "../contrib/xxhash/xxhash.h"
 
 using namespace Halley;
@@ -24,25 +26,60 @@ Hash::Hasher::Hasher()
 {
 	auto d = XXH3_createState();
 	XXH3_64bits_reset(d);
-	data = d;
+	state = d;
 }
 
 Hash::Hasher::~Hasher()
 {
-	XXH3_freeState(static_cast<XXH3_state_t*>(data));
-}
-
-void Hash::Hasher::feedBytes(gsl::span<const std::byte> bytes)
-{
-	XXH3_64bits_update(static_cast<XXH3_state_t*>(data), bytes.data(), size_t(bytes.size_bytes()));
+	XXH3_freeState(static_cast<XXH3_state_t*>(state));
 }
 
 uint64_t Hash::Hasher::digest()
 {
-	return XXH3_64bits_digest(static_cast<XXH3_state_t*>(data));
+	return XXH3_64bits_digest(static_cast<XXH3_state_t*>(state));
 }
 
 void Hash::Hasher::reset()
 {
-	XXH3_64bits_reset(static_cast<XXH3_state_t*>(data));
+	XXH3_64bits_reset(static_cast<XXH3_state_t*>(state));
+}
+
+void Hash::Hasher::feedBytes(gsl::span<const std::byte> bytes)
+{
+	XXH3_64bits_update(static_cast<XXH3_state_t*>(state), bytes.data(), bytes.size_bytes());
+}
+
+void Hash::Hasher::feed1Byte(const void* bytes)
+{
+	XXH3_64bits_update(static_cast<XXH3_state_t*>(state), bytes, 1);
+}
+
+void Hash::Hasher::feed2Bytes(const void* bytes)
+{
+	XXH3_64bits_update(static_cast<XXH3_state_t*>(state), bytes, 2);
+}
+
+void Hash::Hasher::feed4Bytes(const void* bytes)
+{
+	XXH3_64bits_update(static_cast<XXH3_state_t*>(state), bytes, 4);
+}
+
+void Hash::Hasher::feed8Bytes(const void* bytes)
+{
+	XXH3_64bits_update(static_cast<XXH3_state_t*>(state), bytes, 8);
+}
+
+void Hash::Hasher::feed16Bytes(const void* bytes)
+{
+	XXH3_64bits_update(static_cast<XXH3_state_t*>(state), bytes, 16);
+}
+
+void Hash::Hasher::feed32Bytes(const void* bytes)
+{
+	XXH3_64bits_update(static_cast<XXH3_state_t*>(state), bytes, 32);
+}
+
+void Hash::Hasher::feed64Bytes(const void* bytes)
+{
+	XXH3_64bits_update(static_cast<XXH3_state_t*>(state), bytes, 64);
 }
