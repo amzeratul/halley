93df3c3996d0e6fd3d313f0b24772185b2420fca
Rodrigo Braz Monteiro <archmagezeratul@gmail.com>
2026-08-04 12:09:33 +0100

    ColourComponent knows if it's been added dynamically

--- shared_gen/cpp/components/colour_component.h
+++ shared_gen/cpp/components/colour_component.h
@@ -20,6 +20,7 @@ public:
 	Halley::Colour4f colour{ "#FFFFFF" };
 	float intensity{ 1 };
 	bool propagate{ true };
+	bool addedDynamically{ false };
 
 	ColourComponent() {
 	}
@@ -37,6 +38,7 @@ public:
 		Halley::EntityConfigNodeSerializer<decltype(colour)>::serialize(colour, Halley::Colour4f{ "#FFFFFF" }, _context, _node, componentName, "colour", makeMask(Type::Prefab, Type::Dynamic, Type::Network));
 		Halley::EntityConfigNodeSerializer<decltype(intensity)>::serialize(intensity, float{ 1 }, _context, _node, componentName, "intensity", makeMask(Type::Prefab, Type::Dynamic, Type::Network));
 		Halley::EntityConfigNodeSerializer<decltype(propagate)>::serialize(propagate, bool{ true }, _context, _node, componentName, "propagate", makeMask(Type::Prefab));
+		Halley::EntityConfigNodeSerializer<decltype(addedDynamically)>::serialize(addedDynamically, bool{ false }, _context, _node, componentName, "addedDynamically", makeMask(Type::SaveData, Type::Dynamic, Type::Network));
 		return _node;
 	}
 
@@ -45,6 +47,7 @@ public:
 		Halley::EntityConfigNodeSerializer<decltype(colour)>::deserialize(colour, Halley::Colour4f{ "#FFFFFF" }, _context, _node, componentName, "colour", makeMask(Type::Prefab, Type::Dynamic, Type::Network));
 		Halley::EntityConfigNodeSerializer<decltype(intensity)>::deserialize(intensity, float{ 1 }, _context, _node, componentName, "intensity", makeMask(Type::Prefab, Type::Dynamic, Type::Network));
 		Halley::EntityConfigNodeSerializer<decltype(propagate)>::deserialize(propagate, bool{ true }, _context, _node, componentName, "propagate", makeMask(Type::Prefab));
+		Halley::EntityConfigNodeSerializer<decltype(addedDynamically)>::deserialize(addedDynamically, bool{ false }, _context, _node, componentName, "addedDynamically", makeMask(Type::SaveData, Type::Dynamic, Type::Network));
 	}
 
 	static void sanitize(Halley::ConfigNode& _node, int _mask) {
@@ -52,6 +55,7 @@ public:
 		if ((_mask & makeMask(Type::Prefab, Type::Dynamic, Type::Network)) == 0) _node.removeKey("colour");
 		if ((_mask & makeMask(Type::Prefab, Type::Dynamic, Type::Network)) == 0) _node.removeKey("intensity");
 		if ((_mask & makeMask(Type::Prefab)) == 0) _node.removeKey("propagate");
+		if ((_mask & makeMask(Type::SaveData, Type::Dynamic, Type::Network)) == 0) _node.removeKey("addedDynamically");
 	}
 
 	Halley::ConfigNode serializeField(const Halley::EntitySerializationContext& _context, std::string_view _fieldName) const {
@@ -61,6 +65,9 @@ public:
 		}
 		if (_fieldName == "intensity") {
 			return Halley::ConfigNodeHelper<decltype(intensity)>::serialize(intensity, _context);
+		}
+		if (_fieldName == "addedDynamically") {
+			return Halley::ConfigNodeHelper<decltype(addedDynamically)>::serialize(addedDynamically, _context);
 		}
 		throw Halley::Exception("Unknown or non-serializable field \"" + Halley::String(_fieldName) + "\"", Halley::HalleyExceptions::Entity);
 	}
@@ -75,17 +82,23 @@ public:
 			Halley::ConfigNodeHelper<decltype(intensity)>::deserialize(intensity, _context, _node);
 			return;
 		}
+		if (_fieldName == "addedDynamically") {
+			Halley::ConfigNodeHelper<decltype(addedDynamically)>::deserialize(addedDynamically, _context, _node);
+			return;
+		}
 		throw Halley::Exception("Unknown or non-serializable field \"" + Halley::String(_fieldName) + "\"", Halley::HalleyExceptions::Entity);
 	}
 
 	void serializeNetwork(const Halley::ByteSerializationContext& _context, Halley::Serializer& _serializer) const {
 		Halley::ByteSerializationHelper<decltype(colour)>::serialize(colour, _context, _serializer, componentIndex, "colour");
 		Halley::ByteSerializationHelper<decltype(intensity)>::serialize(intensity, _context, _serializer, componentIndex, "intensity");
+		Halley::ByteSerializationHelper<decltype(addedDynamically)>::serialize(addedDynamically, _context, _serializer, componentIndex, "addedDynamically");
 	}
 
 	void deserializeNetwork(const Halley::ByteSerializationContext& _context, Halley::Deserializer& _deserializer) {
 		Halley::ByteSerializationHelper<decltype(colour)>::deserialize(colour, _context, _deserializer, componentIndex, "colour");
 		Halley::ByteSerializationHelper<decltype(intensity)>::deserialize(intensity, _context, _deserializer, componentIndex, "intensity");
+		Halley::ByteSerializationHelper<decltype(addedDynamically)>::deserialize(addedDynamically, _context, _deserializer, componentIndex, "addedDynamically");
 	}
 
 
